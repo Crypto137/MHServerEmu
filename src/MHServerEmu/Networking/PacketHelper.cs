a7af57492bee66abd0eec2475c154cead370c3b1
Crypto137 <Crypto137@users.noreply.github.com>
2023-07-21 06:39:40 +0300

    Implement message parsing from packet files

---
+++ src/MHServerEmu/Networking/PacketHelper.cs
@@ -0,0 +1,33 @@
+﻿using Google.ProtocolBuffers;
+using MHServerEmu.Common;
+
+namespace MHServerEmu.Networking
+{
+    public static class PacketHelper
+    {
+        public static void ParseServerMessagesFromPacketFile(string fileName)
+        {
+            string path = $"{Directory.GetCurrentDirectory()}\\Assets\\Packets\\{fileName}";
+
+            if (File.Exists(path))
+            {
+                CodedInputStream stream = CodedInputStream.CreateInstance(File.ReadAllBytes(path));
+                ClientPacket packet = new(stream);
+
+                if (packet.Command == MuxCommand.Message)
+                {
+                    foreach (GameMessage message in packet.Messages)
+                    {
+                        Console.WriteLine((GameServerToClientMessage)message.Id);
+                        Console.WriteLine(message.Content.ToHexString());
+                        Console.WriteLine();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{fileName} not found");
+            }
+        }
+    }
+}
