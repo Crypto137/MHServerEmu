f07f182b5e603eb7535e0957c8ebb5cb5c308b29
Crypto137 <Crypto137@users.noreply.github.com>
2023-07-26 08:28:03 +0300

    Add enum for region prototype ids

--- src/MHServerEmu/GameServer/Data/Prototypes.cs
+++ src/MHServerEmu/GameServer/Data/Prototypes.cs
@@ -72,4 +72,122 @@ namespace MHServerEmu.GameServer.Data
         Wolverine = 13237838511939982809,
         X23 = 7643048032188437211
     }
+
+    public enum RegionPrototype : ulong
+    {
+        // Hubs
+        AvengersTower = 9142075282174842340,
+        ShieldTrainingRoom = 12181996598405306634,
+        XaviersSchool = 7293929583592937434,
+        ShieldHelicarrier = 13623659297421268224,
+        OdinsPalace = 6777883860663800739,
+        HammerBay = 2398160822904886324,
+        DangerRoom = 13296910602616641976,
+        FurysToolshed = 17072997612720691719,
+
+        // Prologue
+        RaftLandingPad = 11602117409128454445,
+
+        // Chapter 1
+        HellsKitchenSouth = 10115017851235015611,
+        HellsKitchenNorth = 10115017851235015611,
+        AbandonedSubway = 9191439093559269723,
+        NightclubDistrict = 1835346266496899713,
+
+        // Chapter 2
+        ShippingYard = 6121022758926621561,
+        HiddenAimLaboratory = 2777924139474164138,
+        CrabTrapRestaurant = 5976827590009297814,
+        TaskmasterInstitute = 8702251210467252908,
+        CanneryRow = 14132435072208214366,
+        CargoBarge = 3240864148892687230,
+
+        // Chapter 3
+        BuccaneerBeach = 15546930156792977757,
+        BambooForest = 15546930156792977757,
+        Lowtown = 15546930156792977757,
+        ShieldReconPost = 13777669704527323332,
+        HandTower = 3712504169451561124,
+
+        // Chapter 4
+        LowerEastSide = 11922318117493283053,
+        UpperEastSide = 7814783688219433201,
+        FiskTower = 4829776495467370741,
+
+        // Chapter 5
+        WretchedSlum = 13322003842876840585,
+        InvadedWarehouse = 4439163727824297109,
+        RuinedProjects = 13322003842876840585,
+        MorlockUnderground = 13322003842876840585,
+        OldTrainyard = 14943602849314446350,
+
+        // Chapter 6
+        HuntingGrounds = 16979229927281662823,
+        TrainingCamp = 16979229927281662823,
+        OuterCompound = 16979229927281662823,
+        CommandBunker = 14933304278036388919,
+
+        // Chapter 7
+        DinosaurJungle = 3816293063869929975,
+        ShieldScienceOutpost = 3816293063869929975,
+        MutateMarsh = 3816293063869929975,
+        SinisterLab = 15860259876442020487,
+
+        // Chapter 8
+        AimWeaponFacility = 7735172603194383419,
+        HydraIsland = 10124302533162047929,
+        DoomstadtOuterVillage = 5943200060062505421,
+        DoomstadtInnerVillage = 5943200060062505421,
+        CastleDoom = 11854040468777277783,
+
+        // Chapter 9
+        FjordsOfNorway = 5941813295004653614,
+        AncientRuinSite = 5941813295004653614,
+        LowerAsgard = 9690692412445890462,
+
+        // Chapter 10
+        LowtownWarzone = 3163018830455251180,
+        AvengersTowerInvaded = 3941087035061182612,
+        HellsKitchenRooftops = 17036392409364308252,
+        XaviersSchoolInvaded = 10782376165327314768,
+        SwordLandingZone = 9854118440624922758,
+        SwordCommandPost = 9854118440624922758,
+
+        // Terminals
+        DailyBugleTerminal = 6538624689610759291,
+        TimesSquareTerminal = 15168170443837678687,
+        AbandonedSubwayTerminal = 4751118824917181965,
+        KingpinsWarehouseTerminal = 10639961636982236893,
+        TaskmasterInstituteTerminal = 557222731317715831,
+        TheHoodsHideoutTerminal = 16440760549536244379,
+        FiskTowerTerminal = 9270366941605405365,
+        ChurchOfPurificationTerminal = 558746960142673599,
+        StrykerCommandBunkerTerminal = 4777309101647144454,
+        SinisterLabTerminal = 16531055526971909292,
+        AimWeaponFacilityTerminal = 3134677553534149649,
+        HydraIslandTerminal = 5316629336354333727,
+        CastleDoomTerminal = 13699997676891218767,
+        OdinsPalaceTerminal = 1435000301050209520,
+        SkrullInvasionTerminal = 4418386810606266519,
+        DimensionsCollideTerminal = 7200960875039369001,
+
+        // One Shot Stories
+        BronxZoo = 5807424830177093216,
+        MarchToAxis = 16604626599937322846,
+        VibraniumMines = 4913805506445059989,
+
+        // Challenges
+        MidtownPatrol = 16748618685203816205,
+        CosmicTrial = 11861535639759165347,
+        MidtownPatrolCosmic = 15044543158919766135,
+        IndustryCityPatrol = 12232311720232100001,
+        IndustryCityPatrolCosmic = 9428362085609710367,
+        HightownPatrol = 9854118440624922758,
+        HightownPatrolCosmic = 3770267243209630478,
+        TheAgeOfUltron = 11707449573185231773,
+        ShieldHoloSim = 1851384890999315356,
+        XDefense = 17669583277812293411,
+        MuspelheimRaid = 943404224811899020,
+        AxisRaid = 10186536050085467400
+    }
 }
