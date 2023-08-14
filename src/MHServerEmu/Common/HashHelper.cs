40e2854317702c348fdbbbdde3c81eff453a0039
Crypto137 <Crypto137@users.noreply.github.com>
2023-08-15 00:55:19 +0300

    Implement random id generation

---
+++ src/MHServerEmu/Common/HashHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+namespace MHServerEmu.Common
+{
+    public static class HashHelper
+    {
+        public static ulong GenerateRandomId()
+        {
+            byte[] hash = MD5.HashData(BitConverter.GetBytes(DateTime.Now.Ticks));
+            return BitConverter.ToUInt64(hash);
+        }
+    }
+}
