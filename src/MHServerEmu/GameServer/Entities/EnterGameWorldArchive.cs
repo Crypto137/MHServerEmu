d8e8375e62831f76394ea6bbf7a1a834c159bba0
Crypto137 <Crypto137@users.noreply.github.com>
2023-08-18 23:05:13 +0300

    Separate LocomotionState from EnterGameWorldArchive

--- src/MHServerEmu/GameServer/Entities/EnterGameWorldArchive.cs
+++ src/MHServerEmu/GameServer/Entities/EnterGameWorldArchive.cs
@@ -12,25 +12,18 @@ namespace MHServerEmu.GameServer.Entities
         // waypoint 010C028043E06BD82AC801
         // something with 0xA0 01BEF8FD06A001B6A501D454902E0094050000
 
-        // known flag values: 0x02 == mini (waypoint, etc), 0x10A0 == players, 0xA0 == ??
+        // known FieldFlags: 0x02 == mini (waypoint, etc), 0x10A0 == players, 0xA0 == ??
 
         private static readonly Logger Logger = LogManager.CreateLogger();
 
         public ulong ReplicationPolicy { get; }
         public ulong EntityId { get; set; }
-        public int LocFlags { get; set; }     // Contains bits that determine fields
+        public int FieldFlags { get; set; }
         public int LocMsgFlags { get; set; }
         public ulong EnumEntityPrototype { get; set; }
         public Vector3 Position { get; set; }
         public Vector3 Orientation { get; set; }
-        public ulong LocBitset { get; set; }
-        public uint LocField8 { get; set; }
-        public float LocField9 { get; set; }    // previously called LocomotionState
-        public uint LocField10 { get; set; }
-        public ulong LocField11 { get; set; }
-        public Vector2 LocField12 { get; set; }
-        public uint LocNaviPath { get; set; }
-        public LocomotionPathNode[] LocomotionPathNodes { get; set; } = Array.Empty<LocomotionPathNode>();
+        public LocomotionState LocomotionState { get; set; }
         public uint UnknownSetting { get; set; }
 
         public EnterGameWorldArchive(byte[] data)
@@ -40,42 +33,21 @@ namespace MHServerEmu.GameServer.Entities
             ReplicationPolicy = stream.ReadRawVarint64();
             EntityId = stream.ReadRawVarint64();
 
-            LocFlags = (int)stream.ReadRawVarint64();
-            LocMsgFlags = LocFlags >> 12;
+            FieldFlags = (int)stream.ReadRawVarint64();
+            LocMsgFlags = FieldFlags >> 12;
 
-            if ((LocFlags & 0x800) > 0) EnumEntityPrototype = stream.ReadRawVarint64();
+            if ((FieldFlags & 0x800) > 0) EnumEntityPrototype = stream.ReadRawVarint64();
 
             Position = new(stream.ReadRawFloat(3),
                 stream.ReadRawFloat(3),
                 stream.ReadRawFloat(3));
 
-            if ((LocFlags & 0x1) > 0)
-            {
+            if ((FieldFlags & 0x1) > 0)
                 Orientation = new(stream.ReadRawFloat(6), stream.ReadRawFloat(6), stream.ReadRawFloat(6));
-            }
             else
-            {
                 Orientation = new(stream.ReadRawFloat(6), 0f, 0f);
-            }
 
-            if ((LocFlags & 0x2) == 0)
-            {
-                if ((LocFlags & 0x8) > 0) LocBitset = stream.ReadRawVarint64();
-                if ((LocFlags & 0x10) > 0) LocField8 = stream.ReadRawVarint32();
-                if ((LocFlags & 0x80) > 0) LocField9 = stream.ReadRawFloat(0);
-                if ((LocFlags & 0x100) > 0) LocField10 = stream.ReadRawVarint32();
-                if ((LocFlags & 0x200) > 0) LocField11 = stream.ReadRawVarint64();
-                if ((LocFlags & 0x400) > 0) LocField12 = new(stream.ReadRawFloat(0), stream.ReadRawFloat(0));
-
-                if ((LocFlags & 0x20) > 0)
-                {
-                    LocNaviPath = stream.ReadRawVarint32();
-                    LocomotionPathNodes = new LocomotionPathNode[stream.ReadRawVarint64()];
-                    for (int i = 0; i < LocomotionPathNodes.Length; i++)
-                        LocomotionPathNodes[i] = new(stream);
-                }
-            }
-
+            if ((FieldFlags & 0x2) == 0) LocomotionState = new(stream, FieldFlags);
             if ((LocMsgFlags & 0x1) > 0) UnknownSetting = stream.ReadRawVarint32();
 
             if (stream.IsAtEnd == false) Logger.Warn("Archive contains unknown fields!");
@@ -85,22 +57,19 @@ namespace MHServerEmu.GameServer.Entities
         {
             ReplicationPolicy = 0x01;
             EntityId = entityId;
-            LocFlags = 0x02;
+            FieldFlags = 0x02;
             Position = position;
             Orientation = new(orientation, 0f, 0f);
         }
 
-        public EnterGameWorldArchive(ulong entityId, Vector3 position, float orientation, float locField9)
+        public EnterGameWorldArchive(ulong entityId, Vector3 position, float orientation, float moveSpeed)
         {
             ReplicationPolicy = 0x01;
             EntityId = entityId;
-            LocFlags = 0x10A0;
+            FieldFlags = 0x10A0;
             Position = position;
             Orientation = new(orientation, 0f, 0f);
-            LocField9 = locField9;
-
-            LocNaviPath = 0;
-            LocomotionPathNodes = new LocomotionPathNode[0];
+            LocomotionState = new(moveSpeed);
             UnknownSetting = 1;
         }
 
@@ -112,33 +81,17 @@ namespace MHServerEmu.GameServer.Entities
 
                 stream.WriteRawVarint64(ReplicationPolicy);
                 stream.WriteRawVarint64(EntityId);
-                stream.WriteRawVarint64((ulong)LocFlags);
+                stream.WriteRawVarint64((ulong)FieldFlags);
 
-                if ((LocFlags & 0x800) > 0) stream.WriteRawVarint64(EnumEntityPrototype);
+                if ((FieldFlags & 0x800) > 0) stream.WriteRawVarint64(EnumEntityPrototype);
                 stream.WriteRawBytes(Position.Encode(3));
 
-                if ((LocFlags & 0x1) > 0)
+                if ((FieldFlags & 0x1) > 0)
                     stream.WriteRawBytes(Orientation.Encode(6));
                 else
                     stream.WriteRawFloat(Orientation.X, 6);
 
-                if ((LocFlags & 0x2) == 0)
-                {
-                    if ((LocFlags & 0x8) > 0) stream.WriteRawVarint64(LocBitset);
-                    if ((LocFlags & 0x10) > 0) stream.WriteRawVarint32(LocField8);
-                    if ((LocFlags & 0x80) > 0) stream.WriteRawFloat(LocField9, 0);
-                    if ((LocFlags & 0x100) > 0) stream.WriteRawVarint32(LocField10);
-                    if ((LocFlags & 0x200) > 0) stream.WriteRawVarint64(LocField11);
-                    if ((LocFlags & 0x400) > 0) stream.WriteRawBytes(LocField12.Encode(0));
-
-                    if ((LocFlags & 0x20) > 0)
-                    {
-                        stream.WriteRawVarint32(LocNaviPath);
-                        stream.WriteRawVarint64((ulong)LocomotionPathNodes.Length);
-                        foreach (LocomotionPathNode naviVector in LocomotionPathNodes) stream.WriteRawBytes(naviVector.Encode());
-                    }
-                }
-
+                if ((FieldFlags & 0x2) == 0) stream.WriteRawBytes(LocomotionState.Encode(FieldFlags));
                 if ((LocMsgFlags & 0x1) > 0) stream.WriteRawVarint32(UnknownSetting);
 
                 stream.Flush();
@@ -153,18 +106,11 @@ namespace MHServerEmu.GameServer.Entities
             {
                 streamWriter.WriteLine($"ReplicationPolicy: 0x{ReplicationPolicy.ToString("X")}");
                 streamWriter.WriteLine($"EntityId: 0x{EntityId.ToString("X")}");
-                streamWriter.WriteLine($"LocFlags: 0x{LocFlags.ToString("X")}");
+                streamWriter.WriteLine($"FieldFlags: 0x{FieldFlags.ToString("X")}");
                 streamWriter.WriteLine($"EnumEntityPrototype: 0x{EnumEntityPrototype.ToString("X")}");
                 streamWriter.WriteLine($"Position: {Position}");
                 streamWriter.WriteLine($"Orientation: {Orientation}");
-                streamWriter.WriteLine($"LocBitSet: 0x{LocBitset.ToString("X")}");
-                streamWriter.WriteLine($"LocField8: 0x{LocField8.ToString("X")}");
-                streamWriter.WriteLine($"LocField9: {LocField9}");
-                streamWriter.WriteLine($"LocField10: 0x{LocField10.ToString("X")}");
-                streamWriter.WriteLine($"LocField11: 0x{LocField11.ToString("X")}");
-                streamWriter.WriteLine($"LocField12: {LocField12}");
-                streamWriter.WriteLine($"LocNaviPath: 0x{LocNaviPath.ToString("X")}");
-                for (int i = 0; i < LocomotionPathNodes.Length; i++) streamWriter.WriteLine($"LocomotionPathNode{i}: {LocomotionPathNodes[i]}");
+                streamWriter.WriteLine($"LocomotionState: {LocomotionState}");
                 streamWriter.WriteLine($"UnknownSetting: 0x{UnknownSetting.ToString("X")}");
 
                 streamWriter.Flush();
---
+++ src/MHServerEmu/GameServer/Entities/LocomotionState.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+using Google.ProtocolBuffers;
+using MHServerEmu.Common;
+using MHServerEmu.GameServer.Common;
+
+namespace MHServerEmu.GameServer.Entities
+{
+    public class LocomotionState
+    {
+        public ulong LocomotionFlags { get; set; }
+        public uint Method { get; set; }
+        public float MoveSpeed { get; set; }
+        public uint Height { get; set; }
+        public ulong FollowEntityId { get; set; }
+        public Vector2 FollowEntityRange { get; set; }
+        public uint PathNodeUInt { get; set; }
+        public LocomotionPathNode[] LocomotionPathNodes { get; set; } = Array.Empty<LocomotionPathNode>();
+
+        public LocomotionState(CodedInputStream stream, int fieldFlags)
+        {
+            if ((fieldFlags & 0x8) > 0) LocomotionFlags = stream.ReadRawVarint64();
+            if ((fieldFlags & 0x10) > 0) Method = stream.ReadRawVarint32();
+            if ((fieldFlags & 0x80) > 0) MoveSpeed = stream.ReadRawFloat(0);
+            if ((fieldFlags & 0x100) > 0) Height = stream.ReadRawVarint32();
+            if ((fieldFlags & 0x200) > 0) FollowEntityId = stream.ReadRawVarint64();
+            if ((fieldFlags & 0x400) > 0) FollowEntityRange = new(stream.ReadRawFloat(0), stream.ReadRawFloat(0));
+
+            if ((fieldFlags & 0x20) > 0)
+            {
+                PathNodeUInt = stream.ReadRawVarint32();
+                LocomotionPathNodes = new LocomotionPathNode[stream.ReadRawVarint64()];
+                for (int i = 0; i < LocomotionPathNodes.Length; i++)
+                    LocomotionPathNodes[i] = new(stream);
+            }
+        }
+
+        public LocomotionState(float moveSpeed)
+        {
+            MoveSpeed = moveSpeed;
+        }
+
+        public LocomotionState(ulong locomotionFlags, uint method, float moveSpeed, uint height,
+            ulong followEntityId, Vector2 followEntityRange, uint pathNodeUInt, LocomotionPathNode[] locomotionPathNodes)
+        {
+            LocomotionFlags = locomotionFlags;
+            Method = method;
+            MoveSpeed = moveSpeed;
+            Height = height;
+            FollowEntityId = followEntityId;
+            FollowEntityRange = followEntityRange;
+            PathNodeUInt = pathNodeUInt;
+            LocomotionPathNodes = locomotionPathNodes;
+        }
+
+        public byte[] Encode(int fieldFlags)
+        {
+            using (MemoryStream memoryStream = new())
+            {
+                CodedOutputStream stream = CodedOutputStream.CreateInstance(memoryStream);
+
+                if ((fieldFlags & 0x8) > 0) stream.WriteRawVarint64(LocomotionFlags);
+                if ((fieldFlags & 0x10) > 0) stream.WriteRawVarint32(Method);
+                if ((fieldFlags & 0x80) > 0) stream.WriteRawFloat(MoveSpeed, 0);
+                if ((fieldFlags & 0x100) > 0) stream.WriteRawVarint32(Height);
+                if ((fieldFlags & 0x200) > 0) stream.WriteRawVarint64(FollowEntityId);
+                if ((fieldFlags & 0x400) > 0) stream.WriteRawBytes(FollowEntityRange.Encode(0));
+
+                if ((fieldFlags & 0x20) > 0)
+                {
+                    stream.WriteRawVarint32(PathNodeUInt);
+                    stream.WriteRawVarint64((ulong)LocomotionPathNodes.Length);
+                    foreach (LocomotionPathNode naviVector in LocomotionPathNodes) stream.WriteRawBytes(naviVector.Encode());
+                }
+
+                stream.Flush();
+                return memoryStream.ToArray();
+            }
+        }
+
+        public override string ToString()
+        {
+            using (MemoryStream memoryStream = new())
+            using (StreamWriter streamWriter = new(memoryStream))
+            {
+                streamWriter.WriteLine($"LocomotionFlags: 0x{LocomotionFlags.ToString("X")}");
+                streamWriter.WriteLine($"Method: 0x{Method.ToString("X")}");
+                streamWriter.WriteLine($"MoveSpeed: {MoveSpeed}");
+                streamWriter.WriteLine($"Height: 0x{Height.ToString("X")}");
+                streamWriter.WriteLine($"FollowEntityId: 0x{FollowEntityId.ToString("X")}");
+                streamWriter.WriteLine($"FollowEntityRange: {FollowEntityRange}");
+                streamWriter.WriteLine($"PathNodeUInt: 0x{PathNodeUInt.ToString("X")}");
+                for (int i = 0; i < LocomotionPathNodes.Length; i++) streamWriter.WriteLine($"LocomotionPathNode{i}: {LocomotionPathNodes[i]}");
+
+                streamWriter.Flush();
+                return Encoding.UTF8.GetString(memoryStream.ToArray());
+            }
+        }
+    }
+}
