772f09a35841b98fb8c07d908c37a3f1cda97add
Crypto137 <Crypto137@users.noreply.github.com>
2023-08-29 10:14:46 +0300

    Update .cell parser

---
+++ src/MHServerEmu/GameServer/Common/Aabb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHServerEmu.GameServer.Common
+{
+    public class Aabb
+    {
+        public Vector3 Min { get; set; }
+        public Vector3 Max { get; set; }
+
+        public Aabb(Vector3 min, Vector3 max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public override string ToString() => $"Min:{Min} Max:{Max}";
+    }
+}
--- src/MHServerEmu/GameServer/GameData/Gpak/FileFormats/Cell.cs
+++ src/MHServerEmu/GameServer/GameData/Gpak/FileFormats/Cell.cs
@@ -6,18 +6,20 @@ namespace MHServerEmu.GameServer.GameData.Gpak.FileFormats
     public class Cell
     {
         public uint Header { get; }
-        public uint Type { get; }
-        public uint Field2 { get; }
-        public Vector3 Max { get; }
-        public Vector3 Min { get; }
-        public uint Field5 { get; }
-        public uint Field6 { get; }     // _1ff
-        public uint Field7 { get; }
-        public uint Field8 { get; }
-        public string PrototypeName { get; }
-        public CellAgent[] Transitions { get; }
-        public CellAgent[] Npcs { get; }
-
+        public uint Type1 { get; }
+        public uint ClassId { get; }
+        public Aabb Aabb { get; }
+        public uint Type2 { get; }
+        public uint Walls { get; }
+        public uint FillerEdges { get; }
+        public uint RoadConnections { get; }
+        public string ClientMap { get; }
+        public CellMarker[] InitializeSet { get; }
+        public CellMarker[] MarkerSet { get; }
+        public CellNaviPatchSource NaviPatchSource { get; }
+        public byte IsOffsetInMapFile { get; }
+        public CellHeightMap HeightMap { get; }
+        public ulong[] HotspotPrototypes { get; }
 
         public Cell(byte[] data)
         {
@@ -25,48 +27,153 @@ namespace MHServerEmu.GameServer.GameData.Gpak.FileFormats
             using (BinaryReader reader = new(stream))
             {
                 Header = reader.ReadUInt32();
-                Type = reader.ReadUInt32();
-                Field2 = reader.ReadUInt32();
-                Max = reader.ReadVector3();
-                Min = reader.ReadVector3();
-                Field5 = reader.ReadUInt32();
-                Field6 = reader.ReadUInt32();
-                Field7 = reader.ReadUInt32();
-                Field8 = reader.ReadUInt32();
-                PrototypeName = reader.ReadFixedString32();
+                Type1 = reader.ReadUInt32();
+                ClassId = reader.ReadUInt32();
+                Vector3 max = reader.ReadVector3();
+                Vector3 min = reader.ReadVector3();
+                Aabb = new(max, min);
+                Type2 = reader.ReadUInt32();
+                Walls = reader.ReadUInt32();
+                FillerEdges = reader.ReadUInt32();
+                RoadConnections = reader.ReadUInt32();
+                ClientMap = reader.ReadFixedString32();
 
-                /*
-                Transitions = new CellAgent[reader.ReadInt32()];
-                for (int i = 0; i < Transitions.Length; i++)
-                    Transitions[i] = new(reader);
+                InitializeSet = new CellMarker[reader.ReadInt32()];
+                for (int i = 0; i < InitializeSet.Length; i++)
+                    InitializeSet[i] = new(reader);
 
-                Npcs = new CellAgent[reader.ReadInt32()];
-                for (int i = 0; i < Npcs.Length; i++)
-                    Npcs[i] = new(reader);
-                */
+                MarkerSet = new CellMarker[reader.ReadInt32()];
+                for (int i = 0; i < MarkerSet.Length; i++)
+                    MarkerSet[i] = new(reader);
+
+                NaviPatchSource = new(reader);
+                IsOffsetInMapFile = reader.ReadByte();
+                HeightMap = new(reader);
+
+                HotspotPrototypes = new ulong[reader.ReadUInt32()];
+                for (int i = 0; i < HotspotPrototypes.Length; i++)
+                    HotspotPrototypes[i] = reader.ReadUInt64();
             }
         }
     }
 
-    public class CellAgent
+    public class CellMarker
     {
-        public uint Field0 { get; }
-        public uint Field1 { get; }
-        public uint Field2 { get; }
-        public string PrototypeName { get; }
-        public byte[] Zeroes { get; }
+        public uint ProtoNamehash { get; }
+        public ulong EntityGuid { get; }
+        public string LastKnownEntityName { get; }
+        public ulong Modifier1Guid { get; }
+        public string Modifier1Text { get; }
+        public ulong Modifier2Guid { get; }
+        public string Modifier2Text { get; }
+        public ulong Modifier3Guid { get; }
+        public string Modifier3Text { get; }
+        public uint EncounterSpawnPhase { get; }
+        public byte OverrideSnapToFloor { get; }
+        public byte OverrideSnapToFloorValue { get; }
+        public ulong FilterGuid { get; }
+        public string LastKnownFilterName { get; }
         public Vector3 Position { get; }
         public Vector3 Orientation { get; }
 
-        public CellAgent(BinaryReader reader)
+        public CellMarker(BinaryReader reader)
         {
-            Field0 = reader.ReadUInt32();
-            Field1 = reader.ReadUInt32();
-            Field2 = reader.ReadUInt32();
-            PrototypeName = reader.ReadFixedString32();
-            Zeroes = reader.ReadBytes(42);
+            ProtoNamehash = reader.ReadUInt32();
+            EntityGuid = reader.ReadUInt64();
+            LastKnownEntityName = reader.ReadFixedString32();
+            Modifier1Guid = reader.ReadUInt64();
+            if (Modifier1Guid != 0) Modifier1Text = reader.ReadFixedString32();
+            Modifier2Guid = reader.ReadUInt64();
+            if (Modifier2Guid != 0) Modifier2Text = reader.ReadFixedString32();
+            Modifier3Guid = reader.ReadUInt64();
+            if (Modifier3Guid != 0) Modifier3Text = reader.ReadFixedString32();
+            EncounterSpawnPhase = reader.ReadUInt32();
+            OverrideSnapToFloor = reader.ReadByte();
+            OverrideSnapToFloorValue = reader.ReadByte();
+            FilterGuid = reader.ReadUInt64();
+            LastKnownFilterName = reader.ReadFixedString32();
             Position = reader.ReadVector3();
             Orientation = reader.ReadVector3();
+        }
+    }
+
+    public class CellNaviPatchSource
+    {
+        // PatchFragments
+        public uint NaviPatchCrc { get; }
+        public CellPatch NaviPatch { get; }
+        public CellPatch PropPatch { get; }
+        public float PlayableArea { get; }
+        public float SpawnableArea { get; }
+
+        public CellNaviPatchSource(BinaryReader reader)
+        {
+            NaviPatchCrc = reader.ReadUInt32();
+            NaviPatch = new(reader);
+            PropPatch = new(reader);
+            PlayableArea = reader.ReadSingle();
+            SpawnableArea = reader.ReadSingle();
+        }
+    }
+
+    public class CellPatch
+    {
+        public Vector3[] Points { get; }
+        public CellPatchEdge[] Edges { get; }
+
+        public CellPatch(BinaryReader reader)
+        {
+            Points = new Vector3[reader.ReadUInt32()];
+            for (int i = 0; i < Points.Length; i++)
+                Points[i] = reader.ReadVector3();
+
+            Edges = new CellPatchEdge[reader.ReadUInt32()];
+            for (int i = 0; i < Edges.Length; i++)
+                Edges[i] = new(reader);
+        }
+    }
+
+    public class CellPatchEdge
+    {
+        public uint ProtoNameHash { get; }
+        public uint Index0 { get; }
+        public uint Index1 { get; }
+        public byte[] Flags0 { get; }
+        public byte[] Flags1 { get; }
+
+        public CellPatchEdge(BinaryReader reader)
+        {
+            ProtoNameHash = reader.ReadUInt32();
+            Index0 = reader.ReadUInt32();
+            Index1 = reader.ReadUInt32();
+
+            Flags0 = new byte[reader.ReadUInt32()];
+            for (int i = 0; i < Flags0.Length; i++)
+                Flags0[i] = reader.ReadByte();
+
+            Flags1 = new byte[reader.ReadUInt32()];
+            for (int i = 0; i < Flags1.Length; i++)
+                Flags1[i] = reader.ReadByte();
+        }
+    }
+
+    public class CellHeightMap
+    {
+        public Vector2 HeightMapSize { get; }
+        public ushort[] HeightMapData { get; }
+        public ushort[] HotspotData { get; }
+
+        public CellHeightMap(BinaryReader reader)
+        {
+            HeightMapSize = new(reader.ReadUInt32(), reader.ReadUInt32());
+
+            HeightMapData = new ushort[reader.ReadUInt32()];
+            for (int i = 0; i < HeightMapData.Length; i++)
+                HeightMapData[i] = reader.ReadUInt16();
+
+            HotspotData = new ushort[reader.ReadUInt32()];
+            for (int i = 0; i < HotspotData.Length; i++)
+                HotspotData[i] = reader.ReadUInt16();
         }
     }
 }
