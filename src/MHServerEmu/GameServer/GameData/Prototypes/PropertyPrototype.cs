de0558902b3de95e4980df8349c2d2a883b04420
Crypto137 <Crypto137@users.noreply.github.com>
2023-08-31 12:05:51 +0300

    Add more property mixin information to PropertyInfo

--- src/MHServerEmu/GameServer/GameData/Prototypes/PropertyPrototype.cs
+++ src/MHServerEmu/GameServer/GameData/Prototypes/PropertyPrototype.cs
@@ -5,8 +5,13 @@ namespace MHServerEmu.GameServer.GameData.Prototypes
 {
     public class PropertyPrototype
     {
-        public CalligraphyValueType[] ParamValueTypes { get; } = new CalligraphyValueType[4];
+        private const byte MaxParamCount = 4;
+
+        public CalligraphyValueType ValueType { get; }
+        public object DefaultValue { get; }
         public byte ParamCount { get; } = 0;
+        public CalligraphyValueType[] ParamValueTypes { get; } = new CalligraphyValueType[MaxParamCount];
+        public object[] ParamDefaultValues { get; } = new object[MaxParamCount];
 
         public PropertyPrototype(Prototype prototype)
         {
@@ -14,17 +19,25 @@ namespace MHServerEmu.GameServer.GameData.Prototypes
             {
                 switch (GameDatabase.Calligraphy.PrototypeFieldDict[element.Id])
                 {
+                    case "Value":
+                        ValueType = element.Type;
+                        DefaultValue = element.Value;
+                        break;
                     case "Param0":
                         ParamValueTypes[0] = element.Type;
+                        ParamDefaultValues[0] = element.Value;
                         break;
                     case "Param1":
                         ParamValueTypes[1] = element.Type;
+                        ParamDefaultValues[1] = element.Value;
                         break;
                     case "Param2":
                         ParamValueTypes[2] = element.Type;
+                        ParamDefaultValues[2] = element.Value;
                         break;
                     case "Param3":
                         ParamValueTypes[3] = element.Type;
+                        ParamDefaultValues[3] = element.Value;
                         break;
                 }
             }
