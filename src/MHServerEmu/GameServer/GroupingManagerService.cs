164e8ef4324bbf39be770e575408de812149ffe2
Crypto137 <Crypto137@users.noreply.github.com>
2023-09-07 10:46:51 +0300

    Restrict access to broadcast and metagame chat channels

--- src/MHServerEmu/GameServer/GroupingManagerService.cs
+++ src/MHServerEmu/GameServer/GroupingManagerService.cs
@@ -23,17 +23,27 @@ namespace MHServerEmu.GameServer
             {
                 case ClientToGameServerMessage.NetMessageChat:
                     var chatMessageIn = NetMessageChat.ParseFrom(message.Content);
-                    Logger.Trace(NetMessageChat.ParseFrom(message.Content).ToString());
 
                     if (CommandManager.TryParse(chatMessageIn.TheMessage.Body, client) == false)
                     {
-                        var chatMessageOut = ChatNormalMessage.CreateBuilder()
-                            .SetRoomType(chatMessageIn.RoomType)
-                            .SetFromPlayerName(client.Session.Account.PlayerData.PlayerName)
-                            .SetTheMessage(chatMessageIn.TheMessage)
-                            .Build();
+                        // Limit broadcast and metagame channels to users with moderator privileges and higher
+                        if ((chatMessageIn.RoomType == ChatRoomTypes.CHAT_ROOM_TYPE_BROADCAST_ALL_SERVERS || chatMessageIn.RoomType == ChatRoomTypes.CHAT_ROOM_TYPE_METAGAME)
+                            && client.Session.Account.UserLevel < Frontend.Accounts.AccountUserLevel.Moderator)
+                        {
+                            client.SendMessage(1, new(NetMessageChatError.CreateBuilder().SetErrorMessage(ChatErrorMessages.CHAT_ERROR_COMMAND_NOT_RECOGNIZED).Build()));
+                        }
+                        else
+                        {
+                            Logger.Trace($"[{chatMessageIn.RoomType}] [{client.Session.Id} ({client.Session.Account.Email})]: {chatMessageIn.TheMessage.Body}");
 
-                        _gameServerManager.FrontendService.BroadcastMessage(2, new(chatMessageOut));
+                            var chatMessageOut = ChatNormalMessage.CreateBuilder()
+                                .SetRoomType(chatMessageIn.RoomType)
+                                .SetFromPlayerName(client.Session.Account.PlayerData.PlayerName)
+                                .SetTheMessage(chatMessageIn.TheMessage)
+                                .Build();
+
+                            _gameServerManager.FrontendService.BroadcastMessage(2, new(chatMessageOut));
+                        }
                     }
 
                     break;
