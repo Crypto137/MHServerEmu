ef21198af2de02605e4589b941b8bcca93ac3491
Crypto137 <Crypto137@users.noreply.github.com>
2023-09-16 18:28:09 +0300

    Implement .string file parser

--- src/MHServerEmu/Common/Extensions/BinaryReaderExtensions.cs
+++ src/MHServerEmu/Common/Extensions/BinaryReaderExtensions.cs
@@ -15,6 +15,35 @@ namespace MHServerEmu.Common.Extensions
             return Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt32()));
         }
 
+        /// <summary>
+        /// Read a null-terminated string at the current position.
+        /// </summary>
+        public static string ReadNullTerminatedString(this BinaryReader reader)
+        {
+            StringBuilder sb = new();
+
+            while (true)
+            {
+                byte b = reader.ReadByte();
+                if (b == 0x00) break;
+                sb.Append((char)b);
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Read a null-terminated string at the specified offset.
+        /// </summary>
+        public static string ReadNullTerminatedString(this BinaryReader reader, long offset)
+        {
+            long pos = reader.BaseStream.Position;              // Remember the current position
+            reader.BaseStream.Seek(offset, 0);                  // Move to the offset
+            string result = reader.ReadNullTerminatedString();  // Read the string
+            reader.BaseStream.Seek(pos, 0);                     // Return to the original position
+            return result;
+        }
+
         public static Vector2 ReadVector2(this BinaryReader reader)
         {
             return new Vector2(reader.ReadSingle(), reader.ReadSingle());
