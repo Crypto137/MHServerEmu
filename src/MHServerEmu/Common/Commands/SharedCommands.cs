1d9d06b7847f9f7695eef7a426705f544f49c2a5
Crypto137 <Crypto137@users.noreply.github.com>
2023-09-24 21:33:13 +0300

    Implement region lookup

--- src/MHServerEmu/Common/Commands/SharedCommands.cs
+++ src/MHServerEmu/Common/Commands/SharedCommands.cs
@@ -6,7 +6,7 @@ using MHServerEmu.Networking;
 
 namespace MHServerEmu.Common.Commands
 {
-    [CommandGroup("lookup", "Searches for prototype id by name.\nUsage: lookup [costume] [pattern]", AccountUserLevel.User)]
+    [CommandGroup("lookup", "Searches for prototype id by name.\nUsage: lookup [costume|region] [pattern]", AccountUserLevel.User)]
     public class LookupCommands : CommandGroup
     {
         [Command("costume", "Usage: lookup costume [pattern]", AccountUserLevel.User)]
@@ -16,30 +16,59 @@ namespace MHServerEmu.Common.Commands
             if (@params.Length == 0) return "Invalid arguments. Type 'help lookup costume' to get help.";
 
             // Find matches for the given pattern
-            List<DataDirectoryPrototypeEntry> matches = new();
+            List<DataDirectoryPrototypeEntry> matchList = new();
             string pattern = @params[0].ToLower();
 
             foreach (DataDirectoryPrototypeEntry entry in GameDatabase.Calligraphy.PrototypeDirectory.Entries)
             {
                 if (entry.FilePath.Contains("Entity/Items/Costumes/Prototypes/") && entry.FilePath.ToLower().Contains(pattern))
-                    matches.Add(entry);
+                    matchList.Add(entry);
             }
 
             // Output
-            if (matches.Count > 0)
+            return OutputPrototypeLookup(matchList, "Entity/Items/Costumes/Prototypes/", client);
+        }
+
+        [Command("region", "Usage: lookup region [pattern]", AccountUserLevel.User)]
+        public string Region(string[] @params, FrontendClient client)
+        {
+            if (@params == null) return Fallback();
+            if (@params.Length == 0) return "Invalid arguments. Type 'help lookup region' to get help.";
+
+            // Find matches for the given pattern
+            List<DataDirectoryPrototypeEntry> matchList = new();
+            string pattern = @params[0].ToLower();
+
+            foreach (DataDirectoryPrototypeEntry entry in GameDatabase.Calligraphy.PrototypeDirectory.Entries)
             {
-                if (client == null)     
+                if (entry.FilePath.Contains("Regions/"))
+                {
+                    string fileName = Path.GetFileName(entry.FilePath);
+                    if (fileName.Contains("Region") && Path.GetExtension(fileName) == ".prototype" && fileName.ToLower().Contains(pattern))
+                        matchList.Add(entry);
+                }
+            }
+
+            // Output
+            return OutputPrototypeLookup(matchList, "Regions/", client);
+        }
+
+        private static string OutputPrototypeLookup(List<DataDirectoryPrototypeEntry> matchList, string rootDirectory, FrontendClient client)
+        {
+            if (matchList.Count > 0)
+            {
+                if (client == null)
                 {
                     // Output as a single string with line breaks if the command was invoked from the console
-                    return matches.Aggregate("Costume Matches:\n",
-                        (current, match) => $"{current}[{match.Id}] {Path.GetRelativePath("Entity/Items/Costumes/Prototypes/", match.FilePath)}\n");
+                    return matchList.Aggregate("Lookup Matches:\n",
+                        (current, match) => $"{current}[{match.Id}] {Path.GetRelativePath(rootDirectory, match.FilePath)}\n");
                 }
-                else                    
+                else
                 {
                     // Output as a list of chat messages if the command was invoked from the in-game chat
                     // This is because the chat window doesn't handle individual messages with too many lines well (e.g. when the lookup pattern is not specific enough)
-                    List<string> outputList = new() { "Costume Matches:" };
-                    outputList.AddRange(matches.Select(match => $"[{match.Id}] {Path.GetRelativePath("Entity/Items/Costumes/Prototypes/", match.FilePath)}"));
+                    List<string> outputList = new() { "Lookup Matches:" };
+                    outputList.AddRange(matchList.Select(match => $"[{match.Id}] {Path.GetRelativePath(rootDirectory, match.FilePath)}"));
                     GroupingManagerService.SendMetagameChatMessages(client, outputList);
                     return string.Empty;
                 }
