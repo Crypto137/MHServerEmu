d5b9f370400197e6785d9d0a0acdbd3c57948583
Crypto137 <Crypto137@users.noreply.github.com>
2023-09-28 22:55:25 +0300

    Minor logger cleanup

--- src/MHServerEmu/Common/Logging/Logger.cs
+++ src/MHServerEmu/Common/Logging/Logger.cs
@@ -12,22 +12,19 @@
             Fatal
         }
 
-        public string Name { get; protected set; }
+        public string Name { get; }
 
         public Logger(string name)
         {
             Name = name;
         }
 
-        private void Log(Level level, string message) => LogRouter.RouteMessage(level, Name, message);
-        private void LogException(Level level, string message, Exception exception) => LogRouter.RouteException(level, Name, message, exception);
-
-        public void Trace(string message) { Log(Level.Trace, message); }
-        public void Debug(string message) { Log(Level.Debug, message); }
-        public void Info(string message) { Log(Level.Info, message); }
-        public void Warn(string message) { Log(Level.Warn, message); }
-        public void Error(string message) { Log(Level.Error, message); }
-        public void Fatal(string message) { Log(Level.Fatal, message); }
+        public void Trace(string message) => Log(Level.Trace, message);
+        public void Debug(string message) => Log(Level.Debug, message);
+        public void Info(string message) => Log(Level.Info, message);
+        public void Warn(string message) => Log(Level.Warn, message);
+        public void Error(string message) => Log(Level.Error, message);
+        public void Fatal(string message) => Log(Level.Fatal, message);
 
         public void TraceException(Exception exception, string message) => LogException(Level.Trace, message, exception);
         public void DebugException(Exception exception, string message) => LogException(Level.Debug, message, exception);
@@ -35,5 +32,8 @@
         public void WarnException(Exception exception, string message) => LogException(Level.Warn, message, exception);
         public void ErrorException(Exception exception, string message) => LogException(Level.Error, message, exception);
         public void FatalException(Exception exception, string message) => LogException(Level.Fatal, message, exception);
+
+        private void Log(Level level, string message) => LogRouter.RouteMessage(level, Name, message);
+        private void LogException(Level level, string message, Exception exception) => LogRouter.RouteException(level, Name, message, exception);
     }
 }
