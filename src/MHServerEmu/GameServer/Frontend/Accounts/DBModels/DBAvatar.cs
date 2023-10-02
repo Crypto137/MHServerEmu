a39298dfc6d4d52b5bf8fa89ea3af0052c1d533b
Crypto137 <Crypto137@users.noreply.github.com>
2023-10-02 06:48:56 +0300

    Adjust raw properties for DB models

--- src/MHServerEmu/GameServer/Frontend/Accounts/DBModels/DBAvatar.cs
+++ src/MHServerEmu/GameServer/Frontend/Accounts/DBModels/DBAvatar.cs
@@ -8,19 +8,18 @@ namespace MHServerEmu.GameServer.Frontend.Accounts.DBModels
         // Because SQLite internally stores all 64 bit values as signed integers, this causes
         // Dapper overflow errors when parsing ulong values larger than 2^63. To avoid this, we
         // create "raw" long properties for each ulong property that actually get saved to and
-        // loaded from the SQLite database. We then cast these raw long values to ulong when we
-        // need to actually access data.
+        // loaded from the SQLite database.
 
         // This isn't required for AccountId, because the first byte in our id is type, and it
         // never goes over 127 to make the account id value larger than 2^63.
 
         public ulong AccountId { get; set; }
 
-        public long RawPrototype { get; private set; }
-        public AvatarPrototype Prototype { get => (AvatarPrototype)RawPrototype; set => RawPrototype = (long)value; }
+        public AvatarPrototype Prototype { get; set; }
+        public long RawPrototype { get => (long)Prototype; private set => Prototype = (AvatarPrototype)value; }
 
-        public long RawCostume { get; private set; }
-        public ulong Costume { get => (ulong)RawCostume; set => RawCostume = (long)value; }
+        public ulong Costume { get; set; }
+        public long RawCostume { get => (long)Costume; private set => Costume = (ulong)value; }
 
         public DBAvatar(ulong accountId, AvatarPrototype prototype)
         {
--- src/MHServerEmu/GameServer/Frontend/Accounts/DBModels/DBPlayer.cs
+++ src/MHServerEmu/GameServer/Frontend/Accounts/DBModels/DBPlayer.cs
@@ -9,19 +9,18 @@ namespace MHServerEmu.GameServer.Frontend.Accounts.DBModels
         // Because SQLite internally stores all 64 bit values as signed integers, this causes
         // Dapper overflow errors when parsing ulong values larger than 2^63. To avoid this, we
         // create "raw" long properties for each ulong property that actually get saved to and
-        // loaded from the SQLite database. We then cast these raw long values to ulong when we
-        // need to actually access data.
+        // loaded from the SQLite database.
 
         // This isn't required for AccountId, because the first byte in our id is type, and it
         // never goes over 127 to make the account id value larger than 2^63.
 
         public ulong AccountId { get; set; }
 
-        public long RawRegion { get; private set; }
-        public RegionPrototype Region { get => (RegionPrototype)RawRegion; set => RawRegion = (long)value; }
+        public RegionPrototype Region { get; set; }
+        public long RawRegion { get => (long)Region; private set => Region = (RegionPrototype)value; }
 
-        public long RawAvatar { get; private set; }
-        public AvatarPrototype Avatar { get => (AvatarPrototype)RawAvatar; set => RawAvatar = (long)value; }
+        public AvatarPrototype Avatar { get; set; }
+        public long RawAvatar { get => (long)Avatar; private set => Avatar = (AvatarPrototype)value; }
 
         public DBPlayer(ulong accountId)
         {
