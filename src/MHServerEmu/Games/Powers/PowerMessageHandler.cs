7ec65d1c8540c4852d7c9d02f6073a7d25523eab
Crypto137 <Crypto137@users.noreply.github.com>
2023-10-23 17:07:34 +0300

    Clean up PowerMessageHandler

--- src/MHServerEmu/Games/Powers/PowerMessageHandler.cs
+++ src/MHServerEmu/Games/Powers/PowerMessageHandler.cs
@@ -19,107 +19,29 @@ namespace MHServerEmu.Games.Powers
             _eventManager = eventManager;
         }
 
-        public List<QueuedGameMessage> HandleMessage(FrontendClient client, GameMessage message)
+        public IEnumerable<QueuedGameMessage> HandleMessage(FrontendClient client, GameMessage message)
         {
-            List<QueuedGameMessage> messageList = new();
-            string powerPrototypePath;
-
             switch ((ClientToGameServerMessage)message.Id)
             {
                 case ClientToGameServerMessage.NetMessageTryActivatePower:
-                    /* ActivatePower using TryActivatePower data
-                    var tryActivatePower = NetMessageTryActivatePower.ParseFrom(message.Content);
-                    ActivatePowerArchive activatePowerArchive = new(tryActivatePowerMessage, client.LastPosition);
-                    client.SendMessage(muxId, new(NetMessageActivatePower.CreateBuilder()
-                        .SetArchiveData(ByteString.CopyFrom(activatePowerArchive.Encode()))
-                        .Build()));
-                    */
-
-                    var tryActivatePower = NetMessageTryActivatePower.ParseFrom(message.Payload);
-
-                    powerPrototypePath = GameDatabase.GetPrototypeName(tryActivatePower.PowerPrototypeId);
-                    Logger.Trace($"Received TryActivatePower for {powerPrototypePath}");
-
-                    if (powerPrototypePath.Contains("ThrowablePowers/"))
-                    {
-                        Logger.Trace($"AddEvent EndThrowing for {tryActivatePower.PowerPrototypeId}");
-                        PrototypeEntry Power = tryActivatePower.PowerPrototypeId.GetPrototype().GetEntry(BlueprintId.Power);
-                        long animationTimeMS = 1100;
-                        if (Power != null)
-                        {
-                            PrototypeEntryElement AnimationTimeMS = Power.GetField(FieldId.AnimationTimeMS);
-                            if (AnimationTimeMS != null) animationTimeMS = (long)AnimationTimeMS.Value;
-                        }
-                        _eventManager.AddEvent(client, EventEnum.EndThrowing, animationTimeMS, tryActivatePower.PowerPrototypeId);
-                        break;
-                    }
-                    else if (powerPrototypePath.Contains("EmmaFrost/"))
-                    {
-                        if (PowerHasKeyword(tryActivatePower.PowerPrototypeId, BlueprintId.DiamondFormActivatePower))
-                            _eventManager.AddEvent(client, EventEnum.DiamondFormActivate, 0, tryActivatePower.PowerPrototypeId);
-                        else if (PowerHasKeyword(tryActivatePower.PowerPrototypeId, BlueprintId.Mental))
-                            _eventManager.AddEvent(client, EventEnum.DiamondFormDeactivate, 0, tryActivatePower.PowerPrototypeId);
-                    }
-                    else if (tryActivatePower.PowerPrototypeId == (ulong)PowerPrototypes.Magik.Ultimate)
-                    {
-                        _eventManager.AddEvent(client, EventEnum.StartMagikUltimate, 0, tryActivatePower.TargetPosition);
-                        _eventManager.AddEvent(client, EventEnum.EndMagikUltimate, 20000, 0u);
-                    }
-
-                    // if (powerPrototypePath.Contains("TravelPower/")) 
-                    //    TrawerPower(client, tryActivatePower.PowerPrototypeId);
-
-                    //Logger.Trace(tryActivatePower.ToString());
-
-                    PowerResultArchive archive = new(tryActivatePower);
-                    if (archive.TargetId > 0)
-                        messageList.Add(new(client, new(NetMessagePowerResult.CreateBuilder()
-                            .SetArchiveData(archive.Serialize())
-                            .Build())));
-
-                    break;
-
+                    return OnTryActivatePower(client, message.Deserialize<NetMessageTryActivatePower>());
+                
                 case ClientToGameServerMessage.NetMessagePowerRelease:
-                    var powerRelease = NetMessagePowerRelease.ParseFrom(message.Payload);
-                    Logger.Trace($"Received PowerRelease for {GameDatabase.GetPrototypeName(powerRelease.PowerPrototypeId)}");
-                    break;
+                    return OnPowerRelease(client, message.Deserialize<NetMessagePowerRelease>());
 
                 case ClientToGameServerMessage.NetMessageTryCancelPower:
-                    var tryCancelPower = NetMessageTryCancelPower.ParseFrom(message.Payload);
-
-                    powerPrototypePath = GameDatabase.GetPrototypeName(tryCancelPower.PowerPrototypeId);
-                    Logger.Trace($"Received TryCancelPower for {powerPrototypePath}");
-
-                    if (powerPrototypePath.Contains("TravelPower/"))
-                    {
-                        _eventManager.AddEvent(client, EventEnum.EndTravel, 0, tryCancelPower.PowerPrototypeId);
-                    }
-
-                    break;
+                    return OnTryCancelPower(client, message.Deserialize<NetMessageTryCancelPower>());
 
                 case ClientToGameServerMessage.NetMessageTryCancelActivePower:
-                    var tryCancelActivePower = NetMessageTryCancelActivePower.ParseFrom(message.Payload);
-                    Logger.Trace("Received TryCancelActivePower");
-                    break;
+                    return OnTryCancelActivePower(client, message.Deserialize<NetMessageTryCancelActivePower>());
 
                 case ClientToGameServerMessage.NetMessageContinuousPowerUpdateToServer:
-                    var continuousPowerUpdate = NetMessageContinuousPowerUpdateToServer.ParseFrom(message.Payload);
-
-                    powerPrototypePath = GameDatabase.GetPrototypeName(continuousPowerUpdate.PowerPrototypeId);
-                    Logger.Trace($"Received ContinuousPowerUpdate for {powerPrototypePath}");
-
-                    if (powerPrototypePath.Contains("TravelPower/"))
-                        HandleTravelPower(client, continuousPowerUpdate.PowerPrototypeId);
-                    // Logger.Trace(continuousPowerUpdate.ToString());
-
-                    break;
+                    return OnContinuousPowerUpdate(client, message.Deserialize<NetMessageContinuousPowerUpdateToServer>());
 
                 default:
                     Logger.Warn($"Received unhandled message {(ClientToGameServerMessage)message.Id} (id {message.Id})");
-                    break;
+                    return Array.Empty<QueuedGameMessage>();
             }
-
-            return messageList;
         }
 
         private bool PowerHasKeyword(ulong PowerId, BlueprintId Keyword)
@@ -157,5 +79,97 @@ namespace MHServerEmu.Games.Powers
                     break;
             }
         }
+
+        #region Message Handling
+
+        private IEnumerable<QueuedGameMessage> OnTryActivatePower(FrontendClient client, NetMessageTryActivatePower tryActivatePower)
+        {
+            /* ActivatePower using TryActivatePower data
+            ActivatePowerArchive activatePowerArchive = new(tryActivatePowerMessage, client.LastPosition);
+            client.SendMessage(muxId, new(NetMessageActivatePower.CreateBuilder()
+                .SetArchiveData(ByteString.CopyFrom(activatePowerArchive.Encode()))
+                .Build()));
+            */
+
+            List<QueuedGameMessage> messageList = new();
+            string powerPrototypePath = GameDatabase.GetPrototypeName(tryActivatePower.PowerPrototypeId);
+            Logger.Trace($"Received TryActivatePower for {powerPrototypePath}");
+
+            if (powerPrototypePath.Contains("ThrowablePowers/"))
+            {
+                Logger.Trace($"AddEvent EndThrowing for {tryActivatePower.PowerPrototypeId}");
+                PrototypeEntry Power = tryActivatePower.PowerPrototypeId.GetPrototype().GetEntry(BlueprintId.Power);
+                long animationTimeMS = 1100;
+                if (Power != null)
+                {
+                    PrototypeEntryElement AnimationTimeMS = Power.GetField(FieldId.AnimationTimeMS);
+                    if (AnimationTimeMS != null) animationTimeMS = (long)AnimationTimeMS.Value;
+                }
+                _eventManager.AddEvent(client, EventEnum.EndThrowing, animationTimeMS, tryActivatePower.PowerPrototypeId);
+                return messageList;
+            }
+            else if (powerPrototypePath.Contains("EmmaFrost/"))
+            {
+                if (PowerHasKeyword(tryActivatePower.PowerPrototypeId, BlueprintId.DiamondFormActivatePower))
+                    _eventManager.AddEvent(client, EventEnum.DiamondFormActivate, 0, tryActivatePower.PowerPrototypeId);
+                else if (PowerHasKeyword(tryActivatePower.PowerPrototypeId, BlueprintId.Mental))
+                    _eventManager.AddEvent(client, EventEnum.DiamondFormDeactivate, 0, tryActivatePower.PowerPrototypeId);
+            }
+            else if (tryActivatePower.PowerPrototypeId == (ulong)PowerPrototypes.Magik.Ultimate)
+            {
+                _eventManager.AddEvent(client, EventEnum.StartMagikUltimate, 0, tryActivatePower.TargetPosition);
+                _eventManager.AddEvent(client, EventEnum.EndMagikUltimate, 20000, 0u);
+            }
+
+            // if (powerPrototypePath.Contains("TravelPower/")) 
+            //    TrawerPower(client, tryActivatePower.PowerPrototypeId);
+
+            //Logger.Trace(tryActivatePower.ToString());
+
+            PowerResultArchive archive = new(tryActivatePower);
+            if (archive.TargetId > 0)
+                messageList.Add(new(client, new(NetMessagePowerResult.CreateBuilder()
+                    .SetArchiveData(archive.Serialize())
+                    .Build())));
+
+            return messageList;
+        }
+
+        private IEnumerable<QueuedGameMessage> OnPowerRelease(FrontendClient client, NetMessagePowerRelease powerRelease)
+        {
+            Logger.Trace($"Received PowerRelease for {GameDatabase.GetPrototypeName(powerRelease.PowerPrototypeId)}");
+            return Array.Empty<QueuedGameMessage>();
+        }
+
+        private IEnumerable<QueuedGameMessage> OnTryCancelPower(FrontendClient client, NetMessageTryCancelPower tryCancelPower)
+        {
+            string powerPrototypePath = GameDatabase.GetPrototypeName(tryCancelPower.PowerPrototypeId);
+            Logger.Trace($"Received TryCancelPower for {powerPrototypePath}");
+
+            if (powerPrototypePath.Contains("TravelPower/"))
+                _eventManager.AddEvent(client, EventEnum.EndTravel, 0, tryCancelPower.PowerPrototypeId);
+
+            return Array.Empty<QueuedGameMessage>();
+        }
+
+        private IEnumerable<QueuedGameMessage> OnTryCancelActivePower(FrontendClient client, NetMessageTryCancelActivePower tryCancelActivePower)
+        {
+            Logger.Trace("Received TryCancelActivePower");
+            return Array.Empty<QueuedGameMessage>();
+        }
+
+        private IEnumerable<QueuedGameMessage> OnContinuousPowerUpdate(FrontendClient client, NetMessageContinuousPowerUpdateToServer continuousPowerUpdate)
+        {
+            string powerPrototypePath = GameDatabase.GetPrototypeName(continuousPowerUpdate.PowerPrototypeId);
+            Logger.Trace($"Received ContinuousPowerUpdate for {powerPrototypePath}");
+
+            if (powerPrototypePath.Contains("TravelPower/"))
+                HandleTravelPower(client, continuousPowerUpdate.PowerPrototypeId);
+            // Logger.Trace(continuousPowerUpdate.ToString());
+
+            return Array.Empty<QueuedGameMessage>();
+        }
+
+        #endregion
     }
 }
