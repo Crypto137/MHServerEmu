c23ed6c0784e3d3cd255fb8d732ba6695d99b326
Crypto137 <Crypto137@users.noreply.github.com>
2023-11-21 20:54:28 +0300

    Remove old flag extensions

--- src/MHServerEmu/Common/Extensions/ArrayExtensions.cs
+++ src/MHServerEmu/Common/Extensions/ArrayExtensions.cs
@@ -100,44 +100,5 @@ namespace MHServerEmu.Common.Extensions
         }
 
         #endregion
-
-        #region uint <-> bool[] Conversion
-
-        /* uint mask cheat sheet for getting bools (1 << i)
-         0 == 0x1,       1 == 0x2,       2 == 0x4,       3 == 0x8,       4 == 0x10,      5 == 0x20,     6 == 0x40,     7 == 0x80,
-         8 == 0x100,     9 == 0x200,    10 == 0x400,    11 == 0x800,    12 == 0x1000,   13 == 0x2000,  14 == 0x4000,  15 == 0x8000
-        16 == 0x10000,  17 == 0x20000,  18 == 0x40000,  19 == 0x80000,  20 == 0x100000
-        */
-
-        /// <summary>
-        /// Converts a uint to a bool array of flags. A uint can hold up to 32 bools.
-        /// </summary>
-        public static bool[] ToBoolArray(this uint value, int arraySize = 32)
-        {
-            if (arraySize > 32) throw new("Cannot decode more than 32 bools from a uint.");
-
-            bool[] output = new bool[arraySize];
-
-            for (int i = 0; i < output.Length; i++)
-                output[i] = (value & (1 << i)) > 0;
-
-            return output;
-        }
-
-        /// <summary>
-        /// Converts a bool array of flags to a uint. A uint can hold up to 32 bools.
-        /// </summary>
-        public static uint ToUInt32(this bool[] boolArray)
-        {
-            if (boolArray.Length > 32) throw new("Cannot encode more than 32 bools in a uint.");
-
-            uint output = 0;
-
-            for (int i = 0; i < boolArray.Length; i++)
-                if (boolArray[i]) output |= (uint)(1 << i);
-
-            return output;
-        }
-        #endregion
     }
 }
