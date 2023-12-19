1ca3262bf2fd2f35b228a009a859c3ada9e90bc0
Crypto137 <Crypto137@users.noreply.github.com>
2023-12-19 06:12:22 +0300

    Minor LogRouter cleanup

--- src/MHServerEmu/Common/Logging/LogRouter.cs
+++ src/MHServerEmu/Common/Logging/LogRouter.cs
@@ -8,24 +8,22 @@ namespace MHServerEmu.Common.Logging
 
         static LogRouter()
         {
-            Task.Run(async () => await RouteMessages());
+            Task.Run(async () => await RouteMessagesAsync());
         }
 
         public static void EnqueueMessage(Logger.Level level, string logger, string message)
         {
             if (LogManager.Enabled == false || LogManager.TargetList.Count == 0) return;
-
-            LogMessage messageToQueue = new(level, logger, message);    // Create a message instance right away to get a more accurate timestamp
-            MessageQueue.Enqueue(messageToQueue);
+            MessageQueue.Enqueue(new(level, logger, message));
         }
 
-        private static async Task RouteMessages()
+        private static async Task RouteMessagesAsync()
         {
             while (true)
             {
-                while (MessageQueue.Any())
+                while (MessageQueue.IsEmpty == false)
                 {
-                    if (MessageQueue.TryDequeue(out var message))
+                    if (MessageQueue.TryDequeue(out LogMessage message))
                     {
                         var targets = LogManager.TargetList.Where(target => (message.Level >= target.MinimumLevel) && (message.Level <= target.MaximumLevel));
                         
