f5e73ce37a2c21a62435aed4c018aea2a1418d51
Crypto137 <Crypto137@users.noreply.github.com>
2023-12-27 14:17:39 +0300

    Implement logger return methods

--- src/MHServerEmu/Common/Logging/Logger.cs
+++ src/MHServerEmu/Common/Logging/Logger.cs
@@ -19,6 +19,7 @@
             Name = name;
         }
 
+        // Normal logging
         public void Trace(string message) => Log(Level.Trace, message);
         public void Debug(string message) => Log(Level.Debug, message);
         public void Info(string message) => Log(Level.Info, message);
@@ -26,6 +27,7 @@
         public void Error(string message) => Log(Level.Error, message);
         public void Fatal(string message) => Log(Level.Fatal, message);
 
+        // Exception logging
         public void TraceException(Exception exception, string message) => LogException(Level.Trace, message, exception);
         public void DebugException(Exception exception, string message) => LogException(Level.Debug, message, exception);
         public void InfoException(Exception exception, string message) => LogException(Level.Info, message, exception);
@@ -33,7 +35,21 @@
         public void ErrorException(Exception exception, string message) => LogException(Level.Error, message, exception);
         public void FatalException(Exception exception, string message) => LogException(Level.Fatal, message, exception);
 
+        // Logging with a return value (for early returns)
+        public T TraceReturn<T>(string message, T returnValue) => LogReturn(Level.Trace, message, returnValue);
+        public T DebugReturn<T>(string message, T returnValue) => LogReturn(Level.Debug, message, returnValue);
+        public T InfoReturn<T>(string message, T returnValue) => LogReturn(Level.Info, message, returnValue);
+        public T WarnReturn<T>(string message, T returnValue) => LogReturn(Level.Warn, message, returnValue);
+        public T ErrorReturn<T>(string message, T returnValue) => LogReturn(Level.Error, message, returnValue);
+        public T FatalReturn<T>(string message, T returnValue) => LogReturn(Level.Fatal, message, returnValue);
+
         private void Log(Level level, string message) => LogRouter.EnqueueMessage(level, Name, message);
         private void LogException(Level level, string message, Exception exception) => Log(level, $"{message} - [Exception] {exception}");
+
+        private T LogReturn<T>(Level level, string message, T returnValue)
+        {
+            Log(level, message);
+            return returnValue;
+        }
     }
 }
