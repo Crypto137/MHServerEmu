f025aaea4e4e7a60ffbd4426b247a5e8e29eeac0
AlexBond <AlexBondLeo@gmail.com>
2024-02-06 18:29:29 +0300

    Move PopulationObjectPrototype

---
+++ src/MHServerEmu/Games/GameData/Prototypes/PopulationObjectPrototype.cs
@@ -0,0 +1,161 @@
+﻿
+namespace MHServerEmu.Games.GameData.Prototypes
+{
+    public class PopulationObjectPrototype : Prototype
+    {
+        public PrototypeId AllianceOverride { get; protected set; }
+        public bool AllowCrossMissionHostility { get; protected set; }
+        public PrototypeId EntityActionTimelineScript { get; protected set; }
+        public EntityFilterSettingsPrototype[] EntityFilterSettings { get; protected set; }
+        public PrototypeId[] EntityFilterSettingTemplates { get; protected set; }
+        public EvalPrototype EvalSpawnProperties { get; protected set; }
+        public FormationTypePrototype Formation { get; protected set; }
+        public PrototypeId FormationTemplate { get; protected set; }
+        public int GameModeScoreValue { get; protected set; }
+        public bool IgnoreBlackout { get; protected set; }
+        public bool IgnoreNaviCheck { get; protected set; }
+        public float LeashDistance { get; protected set; }
+        public PrototypeId OnDefeatLootTable { get; protected set; }
+        public SpawnOrientationTweak OrientationTweak { get; protected set; }
+        public PopulationRiderPrototype[] Riders { get; protected set; }
+        public bool UseMarkerOrientation { get; protected set; }
+        public PrototypeId UsePopulationMarker { get; protected set; }
+        public PrototypeId CleanUpPolicy { get; protected set; }
+    }
+
+    public class PopulationEntityPrototype : PopulationObjectPrototype
+    {
+        public PrototypeId Entity { get; protected set; }
+    }
+
+    public class PopulationClusterFixedPrototype : PopulationObjectPrototype
+    {
+        public PrototypeId[] Entities { get; protected set; }
+        public EntityCountEntryPrototype[] EntityEntries { get; protected set; }
+    }
+
+    public class PopulationClusterPrototype : PopulationObjectPrototype
+    {
+        public short Max { get; protected set; }
+        public short Min { get; protected set; }
+        public float RandomOffset { get; protected set; }
+        public PrototypeId Entity { get; protected set; }
+    }
+
+    public class PopulationClusterMixedPrototype : PopulationObjectPrototype
+    {
+        public short Max { get; protected set; }
+        public short Min { get; protected set; }
+        public float RandomOffset { get; protected set; }
+        public PopulationObjectPrototype[] Choices { get; protected set; }
+    }
+
+    public class PopulationLeaderPrototype : PopulationObjectPrototype
+    {
+        public PrototypeId Leader { get; protected set; }
+        public PopulationObjectPrototype[] Henchmen { get; protected set; }
+    }
+
+    public class PopulationEncounterPrototype : PopulationObjectPrototype
+    {
+        public AssetId EncounterResource { get; protected set; }
+    }
+
+    public class PopulationFormationPrototype : PopulationObjectPrototype
+    {
+        public PopulationRequiredObjectPrototype[] Objects { get; protected set; }
+    }
+
+    public class PopulationRiderEntityPrototype : PopulationRiderPrototype
+    {
+        public PrototypeId Entity { get; protected set; }
+    }
+
+    public class PopulationGroupPrototype : PopulationObjectPrototype
+    {
+        public PopulationObjectPrototype[] EntitiesAndGroups { get; protected set; }
+    }
+
+    public class PopulationRiderPrototype : Prototype
+    {
+    } 
+
+    public class PopulationRiderBlackOutPrototype : PopulationRiderPrototype
+    {
+        public PrototypeId BlackOutZone { get; protected set; }
+    }
+
+    public class PopulationRequiredObjectPrototype : Prototype
+    {
+        public PopulationObjectPrototype Object { get; protected set; }
+        public PrototypeId ObjectTemplate { get; protected set; }
+        public short Count { get; protected set; }
+        public EvalPrototype EvalSpawnProperties { get; protected set; }
+        public PrototypeId RankOverride { get; protected set; }
+        public bool Critical { get; protected set; }
+        public float Density { get; protected set; }
+        public AssetId[] RestrictToCells { get; protected set; }
+        public PrototypeId[] RestrictToAreas { get; protected set; }
+        public PrototypeId RestrictToDifficultyMin { get; protected set; }
+        public PrototypeId RestrictToDifficultyMax { get; protected set; }
+    }
+
+    public class PopulationRequiredObjectListPrototype : Prototype
+    {
+        public PopulationRequiredObjectPrototype[] RequiredObjects { get; protected set; }
+    }
+
+    public class BoxFormationTypePrototype : FormationTypePrototype
+    {
+    }
+
+    public class LineRowInfoPrototype : Prototype
+    {
+        public int Num { get; protected set; }
+        public FormationFacing Facing { get; protected set; }
+    }
+
+    public class LineFormationTypePrototype : FormationTypePrototype
+    {
+        public LineRowInfoPrototype[] Rows { get; protected set; }
+    }
+
+    public class ArcFormationTypePrototype : FormationTypePrototype
+    {
+        public int ArcDegrees { get; protected set; }
+    }
+
+    public class FormationSlotPrototype : FormationTypePrototype
+    {
+        public float X { get; protected set; }
+        public float Y { get; protected set; }
+        public float Yaw { get; protected set; }
+    }
+
+    public class FixedFormationTypePrototype : FormationTypePrototype
+    {
+        public FormationSlotPrototype[] Slots { get; protected set; }
+    }
+
+    public class CleanUpPolicyPrototype : Prototype
+    {
+    }
+
+    public class EntityCountEntryPrototype : Prototype
+    {
+        public PrototypeId Entity { get; protected set; }
+        public int Count { get; protected set; }
+    }
+
+    public class PopulationListTagObjectPrototype : Prototype
+    {
+    }
+
+    public class PopulationListTagEncounterPrototype : Prototype
+    {
+    }
+
+    public class PopulationListTagThemePrototype : Prototype
+    {
+    }
+}
