6b58d620651f4b32b5e8c13b0a04ae4e2cdbfd34
Crypto137 <Crypto137@users.noreply.github.com>
2024-02-22 07:58:37 +0300

    Clean up PrototypePropertyCollection

--- src/MHServerEmu/Games/GameData/Calligraphy/PrototypePropertyCollection.cs
+++ src/MHServerEmu/Games/GameData/Calligraphy/PrototypePropertyCollection.cs
@@ -4,22 +4,37 @@ using MHServerEmu.Games.Properties;
 
 namespace MHServerEmu.Games.GameData.Calligraphy
 {
+    /// <summary>
+    /// A <see cref="PropertyCollection"/> that stores properties deserialized from Calligraphy prototype mixin field groups.
+    /// </summary>
     public class PrototypePropertyCollection : PropertyCollection
     {
         private static readonly Logger Logger = LogManager.CreateLogger();
 
-        private readonly Dictionary<ulong, PropertyId> _mixinPropertyLookup;    // See SetKeyToPropertyId() for details on this
+        // Child prototypes may override params of properties of their parents, so PrototypePropertyCollection has to
+        // keep track of how contained property ids correspond to blueprints. For that purpose it uses a dictionary with
+        // composite values made from blueprint copy number and property enum as keys.
+        private readonly Dictionary<ulong, PropertyId> _mixinPropertyLookup;
 
+        /// <summary>
+        /// Constructs a new blank <see cref="PrototypePropertyCollection"/> instance.
+        /// </summary>
         public PrototypePropertyCollection()
         {
             _mixinPropertyLookup = new();
         }
 
+        /// <summary>
+        /// Constructs a new <see cref="PrototypePropertyCollection"/> instance and copies mixin property lookups from another collection.
+        /// </summary>
         public PrototypePropertyCollection(Dictionary<ulong, PropertyId> mixinPropertyLookup)
         {
             _mixinPropertyLookup = new(mixinPropertyLookup);    // Copy mixin lookups
         }
 
+        /// <summary>
+        /// Clones this <see cref="PrototypePropertyCollection"/>.
+        /// </summary>
         public PrototypePropertyCollection ShallowCopy()
         {
             PrototypePropertyCollection newCollection = new(_mixinPropertyLookup);
@@ -27,55 +42,56 @@ namespace MHServerEmu.Games.GameData.Calligraphy
             return newCollection;
         }
 
-        // The ugliness going on here is an attempt to imitate passing struct pointers C++ style.
-        // TODO: Clean this mess up.
-
+        /// <summary>
+        /// Sets a <see cref="PropertyValue"/> for a <see cref="PropertyId"/> from a mixin field group.
+        /// </summary>
         public void SetPropertyFromMixin(PropertyValue value, PropertyId propertyId, byte blueprintCopyNum, byte paramsSetMask)
         {
             PropertyValue? existingValueRef = null;
-            CurveProperty? curveProperty = null;
-            PropertyId? curveIndex = null;
 
-            SetKeyToPropertyId(ref propertyId, blueprintCopyNum, paramsSetMask, ref existingValueRef, ref curveProperty, ref curveIndex);
+            SetKeyToPropertyId(ref propertyId, blueprintCopyNum, paramsSetMask, ref existingValueRef);
             SetPropertyValue(propertyId, value);
         }
 
+        /// <summary>
+        /// Updates the <see cref="PropertyId"/> of an existing <see cref="PropertyValue"/> set from a mixin field group.
+        /// </summary>
         public void ReplacePropertyIdFromMixin(PropertyId newPropertyId, byte blueprintCopyNum, byte paramsSetMask)
         {
             PropertyValue? existingValueRef = new();
-            CurveProperty? curveProperty = null;
-            PropertyId? curveIndex = null;
 
-            if (SetKeyToPropertyId(ref newPropertyId, blueprintCopyNum, paramsSetMask, ref existingValueRef, ref curveProperty, ref curveIndex))
-                SetPropertyValue(newPropertyId, (PropertyValue)existingValueRef);     // Set property value only if there is something to replace
+            // If the id got updated we need to reassign the existing value to the new id
+            if (SetKeyToPropertyId(ref newPropertyId, blueprintCopyNum, paramsSetMask, ref existingValueRef))
+                SetPropertyValue(newPropertyId, (PropertyValue)existingValueRef);
         }
 
+        /// <summary>
+        /// Sets a curve property for a <see cref="PropertyId"/> from a mixin field group.
+        /// </summary>
         public void SetCurvePropertyFromMixin(PropertyId propertyId, CurveId curveId, PropertyId indexProperty, PropertyInfo info, byte blueprintCopyNum)
         {
-            PropertyValue? existingValueRef = null;
-            CurveProperty? nullableOldCurve = new();
-            PropertyId? nullableIndexProperty = indexProperty;
+            CurveProperty oldCurve = new();
 
-            bool replaced = SetKeyToPropertyId(ref propertyId, blueprintCopyNum, 0xff, ref existingValueRef, ref nullableOldCurve, ref nullableIndexProperty);
+            bool replaced = SetKeyToPropertyId(ref propertyId, blueprintCopyNum, 0xff, ref oldCurve, (PropertyId?)indexProperty);
 
-            var oldCurve = (CurveProperty)nullableOldCurve;
-            indexProperty = (PropertyId)nullableIndexProperty;
-
+            // There must be a valid index property
             if (indexProperty == PropertyId.Invalid)
             {
-                // If the prototype contained an invalid curve index, fall back to the old curve or default curve index from the property info
                 if (replaced)
                 {
                     if (oldCurve.IndexPropertyId == PropertyId.Invalid)
                     {
-                        Logger.Warn("Prototype property read error: trying to replace a curve property that has an invalid curve index");
+                        // Nothing to fall back on
+                        Logger.Error("Prototype property read error: trying to replace a curve property that has an invalid curve index");
                         return;
                     }
 
+                    // If we are replacing a valid existing curve property, get the index property from it
                     indexProperty = oldCurve.IndexPropertyId;
                 }
                 else
                 {
+                    // If we are adding a new property fall back to the default curve defined in property info
                     indexProperty = info.DefaultCurveIndex;
                 }
             }
@@ -83,38 +99,66 @@ namespace MHServerEmu.Games.GameData.Calligraphy
             SetCurveProperty(propertyId, curveId, indexProperty, info, UInt32Flags.None, true);
         }
 
+        /// <summary>
+        /// Updates the <see cref="PropertyId"/> and the curve index property for an existing curve property added from a mixin field group.
+        /// </summary>
         public void ReplaceCurvePropertyIdFromMixin(PropertyId propertyId, PropertyId indexProperty, PropertyInfo info, byte blueprintCopyNum, byte paramsSetMask)
         {
-            PropertyValue? existingValueRef = null;
-            CurveProperty? nullableOldCurve = new();
-            PropertyId? nullableIndexProperty = indexProperty;
+            CurveProperty oldCurve = new();
 
-            if (SetKeyToPropertyId(ref propertyId, blueprintCopyNum, paramsSetMask, ref existingValueRef, ref nullableOldCurve, ref nullableIndexProperty))
-                SetCurveProperty(propertyId, ((CurveProperty)nullableOldCurve).CurveId, (PropertyId)nullableIndexProperty, info, UInt32Flags.None, true);
+            if (SetKeyToPropertyId(ref propertyId, blueprintCopyNum, paramsSetMask, ref oldCurve, (PropertyId?)indexProperty))
+                SetCurveProperty(propertyId, oldCurve.CurveId, indexProperty, info, UInt32Flags.None, true);  
         }
 
+        /// <summary>
+        /// Updates the <see cref="PropertyId"/> for an existing curve property added from a mixin field group.
+        /// </summary>
         public void ReplaceCurvePropertyIdFromMixin(PropertyId propertyId, PropertyInfo info, byte blueprintCopyNum, byte paramsSetMask)
         {
-            PropertyValue? existingValueRef = null;
-            CurveProperty? nullableOldCurve = new();
-            PropertyId? nullableIndexProperty = null;
+            CurveProperty oldCurve = new();
 
-            if (SetKeyToPropertyId(ref propertyId, blueprintCopyNum, paramsSetMask, ref existingValueRef, ref nullableOldCurve, ref nullableIndexProperty))
-            {
-                CurveProperty oldCurve = (CurveProperty)nullableOldCurve;
+            if (SetKeyToPropertyId(ref propertyId, blueprintCopyNum, paramsSetMask, ref oldCurve, null))
                 SetCurveProperty(propertyId, oldCurve.CurveId, oldCurve.IndexPropertyId, info, UInt32Flags.None, true);
-            }
         }
 
-        private bool SetKeyToPropertyId(ref PropertyId propertyIdRef, byte blueprintCopyNum, byte paramsSetMask,
-            ref PropertyValue? existingValueRef, ref CurveProperty? curveProp, ref PropertyId? curveIndex)
+        /// <summary>
+        /// Updates the <see cref="PropertyId"/> corresponding to a <see cref="PropertyEnum"/> / blueprint copy number pair. For use with non-curve properties.
+        /// </summary>
+        private bool SetKeyToPropertyId(ref PropertyId propertyIdRef, byte blueprintCopyNum, byte paramsSetMask, ref PropertyValue? existingValueRef)
         {
-            // TODO: Make an overload of this method for handling curve properties
             bool valueIsReplaced = false;
+            ulong key = ((ulong)blueprintCopyNum << 32) | (ulong)propertyIdRef.Enum;
 
-            // Child prototypes may override params of properties of their parents, so PrototypePropertyCollection has to
-            // keep track of how contained property ids correspond to blueprints. For that purpose it uses a dictionary with
-            // composite values made from blueprint copy number and property enum as keys.
+            // If the lookup dict already has this key it means we are modifying an existing property rather than adding a new one
+            if (_mixinPropertyLookup.TryGetValue(key, out PropertyId existingPropertyId))
+            {
+                if (HasMatchingParams(propertyIdRef, existingPropertyId, 0xff) == false)
+                {
+                    valueIsReplaced = true;
+
+                    // If existingValueRef is not a null ref it means we are replacing an existing property id,
+                    // and we need to output existing PropertyValue to this ref so it can be reassigned to the new id.
+                    //
+                    // For null comparison we need to cast null to one of the supported PropertyValue types
+                    // for this check because of all the implicit casting we are doing.
+                    if (existingValueRef != (bool?)null)
+                        existingValueRef = GetPropertyValue(existingPropertyId);
+
+                    SetOverridenParams(ref propertyIdRef, existingPropertyId, paramsSetMask);
+                    RemoveProperty(existingPropertyId);
+                }
+            }
+
+            _mixinPropertyLookup[key] = propertyIdRef;
+            return valueIsReplaced;
+        }
+
+        /// <summary>
+        /// Updates the <see cref="PropertyId"/> corresponding to a <see cref="PropertyEnum"/> / blueprint copy number pair. For use with curve properties.
+        /// </summary>
+        private bool SetKeyToPropertyId(ref PropertyId propertyIdRef, byte blueprintCopyNum, byte paramsSetMask, ref CurveProperty oldCurve, PropertyId? curveIndex)
+        {
+            bool valueIsReplaced = false;
             ulong key = ((ulong)blueprintCopyNum << 32) | (ulong)propertyIdRef.Enum;
 
             // If the lookup dict already has this key it means we are modifying an existing property rather than adding a new one
@@ -122,28 +166,18 @@ namespace MHServerEmu.Games.GameData.Calligraphy
             {
                 if (HasMatchingParams(propertyIdRef, existingPropertyId, 0xff) == false || curveIndex != null)
                 {
-                    valueIsReplaced = true;
-
-                    // We need to cast null to one of the supported value types for this check because of all the implicit casting we are doing
-                    if (existingValueRef != (bool?)null)
-                        existingValueRef = GetPropertyValue(existingPropertyId);
-
-                    if (curveProp != null)
+                    // If there is an existing CurveProperty assigned to this id and we are actually making changes,
+                    // we need to output this CurveProperty to be reassigned to the new id.
+                    CurveProperty? nullableExistingCurveProp = GetCurveProperty(existingPropertyId);
+                    if (nullableExistingCurveProp != null)
                     {
-                        CurveProperty? nullableExistingCurveProp = GetCurveProperty(existingPropertyId);
-                        if (nullableExistingCurveProp != null)
-                        {
-                            var existingCurveProp = (CurveProperty)nullableExistingCurveProp;
+                        var existingCurveProp = (CurveProperty)nullableExistingCurveProp;
 
-                            if (curveIndex != null && propertyIdRef == existingPropertyId && existingCurveProp.IndexPropertyId == curveIndex)
-                                return false;
+                        if (curveIndex != null && propertyIdRef == existingPropertyId && existingCurveProp.IndexPropertyId == curveIndex)
+                            return false;   // No need to change anything
 
-                            curveProp = existingCurveProp;
-                        }
-                        else
-                        {
-                            valueIsReplaced = false;
-                        }
+                        oldCurve = existingCurveProp;
+                        valueIsReplaced = true;
                     }
 
                     SetOverridenParams(ref propertyIdRef, existingPropertyId, paramsSetMask);
@@ -155,6 +189,9 @@ namespace MHServerEmu.Games.GameData.Calligraphy
             return valueIsReplaced;
         }
 
+        /// <summary>
+        /// Compares parameters of two <see cref="PropertyId"/> values. The provided mask defines params that need to be compared.
+        /// </summary>
         private bool HasMatchingParams(PropertyId left, PropertyId right, byte paramsSetMask)
         {
             if (paramsSetMask == 0xff) return left == right;
@@ -174,6 +211,9 @@ namespace MHServerEmu.Games.GameData.Calligraphy
             return true;
         }
 
+        /// <summary>
+        /// Overrides <see cref="PropertyParam"/> values of a <see cref="PropertyId"/>. The provided mask defines params that need to be overriden.
+        /// </summary>
         private void SetOverridenParams(ref PropertyId destId, PropertyId sourceId, byte paramsSetMask)
         {
             if (paramsSetMask == 0xff) return;
