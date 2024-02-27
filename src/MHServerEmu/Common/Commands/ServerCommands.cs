4fca5321ae504ef8d2e1d6854b29b8507cecebde
Crypto137 <Crypto137@users.noreply.github.com>
2024-02-27 10:07:03 +0300

    Move debug commands to shared

--- src/MHServerEmu/Common/Commands/ServerCommands.cs
+++ src/MHServerEmu/Common/Commands/ServerCommands.cs
@@ -1,12 +1,6 @@
 ﻿using Gazillion;
 using MHServerEmu.Common.Config;
-using MHServerEmu.Common.Logging;
 using MHServerEmu.Frontend;
-using MHServerEmu.Games.Common;
-using MHServerEmu.Games.Entities;
-using MHServerEmu.Games.GameData;
-using MHServerEmu.Games.Generators;
-using MHServerEmu.Grouping;
 using MHServerEmu.Networking;
 using MHServerEmu.PlayerManagement;
 using MHServerEmu.PlayerManagement.Accounts;
@@ -106,87 +100,6 @@ namespace MHServerEmu.Common.Commands
 
             PacketHelper.ParseServerMessagesFromAllPacketFiles();
 
-            return string.Empty;
-        }
-    }
-
-    [CommandGroup("debug", "Debug commands for development.", AccountUserLevel.User)]
-    public class DebugCommands : CommandGroup
-    {
-        private static readonly Logger Logger = LogManager.CreateLogger();
-
-        [Command("test", "Runs test code.", AccountUserLevel.Admin)]
-        public string Test(string[] @params, FrontendClient client)
-        {
-            return string.Empty;
-        }
-
-        [Command("cell", "Shows current cell.", AccountUserLevel.User)]
-        public string Cell(string[] @params, FrontendClient client)
-        {
-            if (client == null) return "You can only invoke this command from the game.";
-            return $"Current cell: {client.AOI.Region.GetCellAtPosition(client.LastPosition).PrototypeName}";
-        }
-
-        [Command("seed", "Shows current seed.", AccountUserLevel.User)]
-        public string Seed(string[] @params, FrontendClient client)
-        {
-            if (client == null) return "You can only invoke this command from the game.";
-            return $"Current seed: {client.AOI.Region.RandomSeed}";
-        }
-
-        [Command("area", "Shows current area.", AccountUserLevel.User)]
-        public string Area(string[] @params, FrontendClient client)
-        {
-            if (client == null) return "You can only invoke this command from the game.";
-            return $"Current area: {client.AOI.Region.GetCellAtPosition(client.LastPosition).Area.PrototypeName}";
-        }
-
-        [Command("near", "Usage: debug near [radius]. Default radius 100.", AccountUserLevel.User)]
-        public string Near(string[] @params, FrontendClient client)
-        {
-            if (client == null) return "You can only invoke this command from the game.";
-
-            if ((@params?.Length > 0 && int.TryParse(@params[0], out int radius)) == false)
-                radius = 100;   // Default to 100 if no radius is specified
-
-            Sphere near = new(client.LastPosition, radius);
-            EntityRegionSPContext context = new() { Flags = EntityRegionSPContextFlags.ActivePartition | EntityRegionSPContextFlags.StaticPartition };
-
-            List<string> entities = new();
-            foreach (var worldEntity in client.AOI.Region.IterateEntitiesInVolume(near, context))
-            {
-                string name = GameDatabase.GetFormattedPrototypeName(worldEntity.BaseData.PrototypeId);
-                ulong entityId = worldEntity.BaseData.EntityId;
-                string status = string.Empty;
-                if (client.AOI.EntityLoaded(entityId) == false) status += "[H]";
-                if (worldEntity is Transition) status += "[T]";
-                if (worldEntity.WorldEntityPrototype.VisibleByDefault == false) status += "[Invis]";
-                entities.Add($"[{entityId}] {name} {status}");
-            }
-
-            if (entities.Count == 0)
-                return "No entities found.";
-
-            ChatHelper.SendMetagameMessage(client, $"Found for R={radius}:");
-            ChatHelper.SendMetagameMessages(client, entities);
-            return string.Empty;
-        }
-
-        [Command("entity", "Displays information about the specified entity.\nUsage: debug entity [EntityId]", AccountUserLevel.User)]
-        public string entity(string[] @params, FrontendClient client)
-        {
-            if (client == null) return "You can only invoke this command from the game.";
-            if (@params == null || @params.Length == 0) return "Invalid arguments. Type 'help debug entity' to get help.";
-
-            if (ulong.TryParse(@params[0], out ulong entityId) == false)
-                return $"Failed to parse EntityId {@params[0]}";
-
-            var entity = client.CurrentGame.EntityManager.GetEntityById(entityId);
-            if (entity == null) return "No entity found.";
-
-            ChatHelper.SendMetagameMessage(client, $"Entity[{entityId}]: {GameDatabase.GetFormattedPrototypeName(entity.BaseData.PrototypeId)}");
-            ChatHelper.SendMetagameMessages(client, entity.Properties.ToString().Split('\n'));
             return string.Empty;
         }
     }
