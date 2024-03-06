bf8c52ab04d5864762451b4d65c867ecdac0722f
Kawaikikinou <tonynogo@gmail.com>
2024-03-06 14:13:00 +0100

    Useless using

--- src/GameDatabaseBrowser/Helpers/SearchHelper.cs
+++ src/GameDatabaseBrowser/Helpers/SearchHelper.cs
@@ -3,13 +3,6 @@ using MHServerEmu.Games.GameData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Threading;
 
 namespace GameDatabaseBrowser.Helpers
 {
