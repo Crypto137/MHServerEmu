b4541cd1dff6ce30d31f9e356c9c43bc42861c19
Crypto137 <Crypto137@users.noreply.github.com>
2024-03-12 20:05:05 +0300

    Separate MHServerEmu.Core into a library

---
+++ src/MHServerEmu.Core/Helpers/CompressionHelper.cs
@@ -0,0 +1,34 @@
+﻿using Free.Ports.zLib;
+using K4os.Compression.LZ4;
+
+namespace MHServerEmu.Core.Helpers
+{
+    public static class CompressionHelper
+    {
+        /// <summary>
+        /// Decompresses the provided LZ4 buffer.
+        /// </summary>
+        public static void LZ4Decode(byte[] source, byte[] target)
+        {
+            LZ4Codec.Decode(source, target);
+        }
+
+        /// <summary>
+        /// Compresses the provided buffer using zlib.
+        /// </summary>
+        public static byte[] ZLibDeflate(byte[] buffer)
+        {
+            // This is used for compressing the achievement database dump before sending it to clients.
+            // We need to compress specifically with a port of zlib rather than various alternatives like SharpZipLib
+            // to produce the same result as the original. zlib.compress() in Python also produces the result we need.
+
+            using (MemoryStream ms = new())
+            using (ZStreamWriter writer = new(ms))
+            {
+                writer.Write(buffer);
+                writer.Close();
+                return ms.ToArray();
+            }
+        }
+    }
+}
--- src/MHServerEmu/Games/Achievements/AchievementDatabase.cs
+++ src/MHServerEmu/Games/Achievements/AchievementDatabase.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics;
 using System.Text.Json;
-using Free.Ports.zLib;
 using Google.ProtocolBuffers;
 using Gazillion;
 using MHServerEmu.Core.Helpers;
@@ -186,19 +185,13 @@ namespace MHServerEmu.Games.Achievements
                 .SetAchievementNewThresholdUS((ulong)AchievementNewThresholdUS.TotalSeconds)
                 .Build().ToByteArray();
 
-            // We need to compress specifically with a port of zlib rather than various alternatives like SharpZipLib
-            // to produce the same result. zlib.compress() in Python also produces the result we need.
+            // NOTE: If you don't use the right library to compress this it's going to cause client-side errors.
+            // See CompressionHelper.ZLibDeflate() for more details.
+            byte[] compressedBuffer = CompressionHelper.ZLibDeflate(dumpBuffer);
 
-            using (MemoryStream ms = new())
-            using (ZStreamWriter writer = new(ms))
-            {
-                writer.Write(dumpBuffer);
-                writer.Close();
-
-               _cachedDump = NetMessageAchievementDatabaseDump.CreateBuilder()
-                    .SetCompressedAchievementDatabaseDump(ByteString.CopyFrom(ms.ToArray()))
-                    .Build();
-            }
+            _cachedDump = NetMessageAchievementDatabaseDump.CreateBuilder()
+                 .SetCompressedAchievementDatabaseDump(ByteString.CopyFrom(compressedBuffer))
+                 .Build();
         }
     }
 }
