801fe47cb8904f7aef061d6f4da646ef2300b56e
AlexBond <AlexBondLeo@gmail.com>
2024-03-21 21:00:13 +0300

    Implement LineRelationship2D

--- src/MHServerEmu.Games/Navi/NaviPoint.cs
+++ src/MHServerEmu.Games/Navi/NaviPoint.cs
@@ -8,16 +8,20 @@ namespace MHServerEmu.Games.Navi
         Attached
     }
 
-    public class NaviPoint
+    public class NaviPoint : IComparable<NaviPoint>
     {
         public Vector3 Pos { get; internal set; }
         public NaviPointFlags Flags { get; private set; }
         public int Influence { get; private set; }
         public float InfluenceRadius { get; private set; }
 
+        private static ulong TotalCount = 0;
+        private readonly ulong _count;
+
         public NaviPoint(Vector3 pos)
         {
             Pos = pos;
+            _count = TotalCount++;
         }
 
         public override string ToString()
@@ -25,5 +29,9 @@ namespace MHServerEmu.Games.Navi
             return $"NaviPoint ({Pos.X:F4} {Pos.Y:F4} {Pos.Z:F4}) flg:{Flags} inf:{Influence}";
         }
 
+        public int CompareTo(NaviPoint other)
+        {
+            return _count.CompareTo(other._count);
+        }
     }
 }
--- src/MHServerEmu.Games/Navi/Pred.cs
+++ src/MHServerEmu.Games/Navi/Pred.cs
@@ -1,4 +1,5 @@
 ﻿using MHServerEmu.Core.VectorMath;
+using RobustPredicates;
 
 namespace MHServerEmu.Games.Navi
 {
@@ -13,6 +14,32 @@ namespace MHServerEmu.Games.Navi
             float xd = (x0 < x1) ? (x1 - x0) : (x0 - x1);
             float yd = (y0 < y1) ? (y1 - y0) : (y0 - y1);
             return (xd * xd + yd * yd) <= 9.0f;
+        }
+
+        public static bool SortInputs<T>(ref T input0, ref T input1) where T : IComparable<T>
+        {
+            if (input0.CompareTo(input1) < 0) 
+                return false;
+            else
+            {
+                (input0, input1) = (input1, input0);
+                return true;
+            }
+        }
+
+        public static double LineRelationship2D(NaviPoint p0, NaviPoint p1, Vector3 pos)
+        {
+            bool flip = SortInputs(ref p0, ref p1);
+            double d = InternalOrient2D(p1.Pos, p0.Pos, pos);
+            return flip ? -d : d;
+        }
+
+        private static double InternalOrient2D(Vector3 v0, Vector3 v1, Vector3 v2)
+        {
+            double[] pa = { v0.X, v0.Y };
+            double[] pb = { v1.X, v1.Y };
+            double[] pc = { v2.X, v2.Y };
+            return Orient2D.Robust(pa, pb, pc);
         }
     }
 }
