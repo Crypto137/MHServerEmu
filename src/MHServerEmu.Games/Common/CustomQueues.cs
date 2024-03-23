6ca93a2f1b8ba5b31bc7d49b6e9ea5dac4e799eb
AlexBond <AlexBondLeo@gmail.com>
2024-03-23 06:24:59 +0300

    FixedPriorityQueue

---
+++ src/MHServerEmu.Games/Common/CustomQueues.cs
@@ -0,0 +1,43 @@
+﻿namespace MHServerEmu.Games.Common
+{
+    public class FixedPriorityQueue<T> where T : IComparable<T>
+    {
+        private List<T> _items;
+
+        public FixedPriorityQueue()
+        {
+            _items = new List<T>();
+        }
+
+        public int Count => _items.Count;
+
+        public void Push(T item)
+        {
+            _items.Add(item);
+            _items.Sort();
+        }
+
+        public T Top()
+        {
+            if (_items.Count == 0)
+                throw new InvalidOperationException("Queue is empty");
+
+            return _items[0];
+        }
+
+        public T Pop()
+        {
+            if (_items.Count == 0)
+                throw new InvalidOperationException("Queue is empty");
+
+            T topItem = _items[0];
+            _items.RemoveAt(0);
+            return topItem;
+        }
+
+        public void Heapify()
+        {
+            _items.Sort();
+        }
+    }
+}
