cc9270811b55d71cf793b8fe954dae59cc73a74b
Crypto137 <Crypto137@users.noreply.github.com>
2024-04-03 22:31:35 +0300

    Update UIWidgetButton implementation

--- src/MHServerEmu.Games/UI/Widgets/UIWidgetButton.cs
+++ src/MHServerEmu.Games/UI/Widgets/UIWidgetButton.cs
@@ -7,7 +7,14 @@ namespace MHServerEmu.Games.UI.Widgets
 {
     public class UIWidgetButton : UISyncData
     {
-        public ulong[] Callbacks { get; set; }      // PlayerGuid
+        // NOTE: This widget may be unfinished. The only place it seems to may have been used is
+        // MetaStateShutdown for DangerRoom, but the TeleportButtonWidget field does not have
+        // any data in any of the prototypes, and even the TeleportButtonWidget prototype itself
+        // does not seem to be referenced anywhere else.
+        //
+        // It does sort of work, but the visuals for it get outside the bounds of the widget bar.
+
+        private readonly List<CallbackBase> _callbackList = new();
 
         public UIWidgetButton(UIDataProvider uiDataProvider, PrototypeId widgetRef, PrototypeId contextRef) : base(uiDataProvider, widgetRef, contextRef) { }
 
@@ -15,26 +22,47 @@ namespace MHServerEmu.Games.UI.Widgets
         {
             base.Decode(stream, boolDecoder);
 
-            Callbacks = new ulong[stream.ReadRawVarint64()];
-            for (int i = 0; i < Callbacks.Length; i++)
-                Callbacks[i] = stream.ReadRawVarint64();
+            uint numCallbacks = stream.ReadRawVarint32();
+            for (uint i = 0; i < numCallbacks; i++)
+                _callbackList.Add(new(stream.ReadRawVarint64()));
         }
 
         public override void Encode(CodedOutputStream stream, BoolEncoder boolEncoder)
         {
             base.Encode(stream, boolEncoder);
 
-            stream.WriteRawVarint64((ulong)Callbacks.Length);
-            for (int i = 0; i < Callbacks.Length; i++)
-                stream.WriteRawVarint64(Callbacks[i]);
+            stream.WriteRawVarint32((uint)_callbackList.Count);
+            foreach (CallbackBase callback in _callbackList)
+                stream.WriteRawVarint64(callback.PlayerGuid);
         }
 
         protected override void BuildString(StringBuilder sb)
         {
             base.BuildString(sb);
 
-            for (int i = 0; i < Callbacks.Length; i++)
-                sb.AppendLine($"Callback{i}: {Callbacks[i]}");
+            for (int i = 0; i < _callbackList.Count; i++)
+                sb.AppendLine($"{nameof(_callbackList)}[{i}]: {_callbackList[i]}");
+        }
+
+        public void AddCallback(ulong playerGuid)
+        {
+            _callbackList.Add(new(playerGuid));
+            UpdateUI();
+        }
+
+        class CallbackBase
+        {
+            public ulong PlayerGuid { get; }
+
+            public CallbackBase(ulong playerGuid)
+            {
+                PlayerGuid = playerGuid;
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(PlayerGuid)}: 0x{PlayerGuid:X16}";
+            }
         }
     }
 }
