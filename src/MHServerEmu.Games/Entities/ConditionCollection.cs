e93a59921b3a59126fa6b09baa4417f437b3b167
Crypto137 <Crypto137@users.noreply.github.com>
2024-04-20 21:43:51 +0300

    Implement more ConditionCollection functionality

--- src/MHServerEmu.Games/Entities/ConditionCollection.cs
+++ src/MHServerEmu.Games/Entities/ConditionCollection.cs
@@ -3,12 +3,19 @@ using Google.ProtocolBuffers;
 using MHServerEmu.Core.Logging;
 using MHServerEmu.Core.Serialization;
 using MHServerEmu.Games.Common;
+using MHServerEmu.Games.GameData;
 using MHServerEmu.Games.Powers;
 
 namespace MHServerEmu.Games.Entities
 {
     public class ConditionCollection : IEnumerable<KeyValuePair<ulong, Condition>>, ISerialize
     {
+        // NOTE: Current state of implementation:
+        // - Adding and removing conditions works, but conditions don't affect their owners ("accrue").
+        // - Condition management is semi-implemented (conditions are currently not aware of the collection they belong to).
+        // - Some data for stacking (e.g. StackId) is implemented, but none of the actual functionality is.
+        // - ConditionCollection::Iterator and ConditionCollection::Handle nested classes are not implemented (do we even need them?).
+
         public const int MaxConditions = 256;
         public const ulong InvalidConditionId = 0;
 
@@ -84,6 +91,56 @@ namespace MHServerEmu.Games.Entities
                 condition.Encode(stream);
         }
 
+        /// <summary>
+        /// Returns the <see cref="Condition"/> with the specified condition id (key).
+        /// Returns <see langword="null"/> if no <see cref="Condition"/> with such id is present in this <see cref="ConditionCollection"/>.
+        /// </summary>
+        public Condition GetCondition(ulong conditionId)
+        {
+            if (_currentConditionDict.TryGetValue(conditionId, out Condition condition) == false)
+                return null;
+
+            return condition;
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Condition"/> with the specified <see cref="PrototypeId"/>.
+        /// Returns <see langword="null"/> if no <see cref="Condition"/> with such prototype is present in this <see cref="ConditionCollection"/>.
+        /// </summary>
+        public Condition GetConditionByRef(PrototypeId conditionRef)
+        {
+            if (conditionRef == PrototypeId.Invalid) return Logger.WarnReturn<Condition>(null, $"GetConditionByRef(): conditionRef == PrototypeId.Invalid");
+
+            foreach (Condition condition in _currentConditionDict.Values)
+            {
+                if (condition.ConditionPrototypeRef == conditionRef)
+                    return condition;
+            }
+
+            return null; 
+        }
+
+        /// <summary>
+        /// Returns the id (key) of the condition with the specified <see cref="PrototypeId"/>.
+        /// Returns invalid id (0) if no condition with such prototype is present in this <see cref="ConditionCollection"/>.
+        /// </summary>
+        public ulong GetConditionIdByRef(PrototypeId conditionRef)
+        {
+            Condition condition = GetConditionByRef(conditionRef);
+            if (condition == null) return InvalidConditionId;
+            return condition.Id;
+        }
+
+        public int GetNumberOfStacks(Condition condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetNumberOfStacks(StackId stackId)
+        {
+            throw new NotImplementedException();
+        }
+
         public bool AddCondition(Condition condition)
         {
             if (condition == null)
@@ -94,6 +151,26 @@ namespace MHServerEmu.Games.Entities
 
             OnInsertCondition(condition);
             return true;
+        }
+
+        public bool RemoveCondition(ulong conditionId)
+        {
+            if (_currentConditionDict.TryGetValue(conditionId, out Condition condition) == false)
+                return false;
+
+            return RemoveCondition(condition);
+        }
+
+        public void RemoveAllConditions(bool removePersistToDB = true)
+        {
+            //if (_owner.Game == null) return;
+
+            // Convert values to an array so that we can remove items from the dict while we iterate
+            foreach (Condition condition in _currentConditionDict.Values.ToArray())
+            {
+                if (removePersistToDB == false && condition.IsPersistToDB()) continue;
+                RemoveCondition(condition);
+            }
         }
 
         public bool HasANegativeStatusEffectCondition()
@@ -127,6 +204,37 @@ namespace MHServerEmu.Games.Entities
         private void OnInsertCondition(Condition condition)
         {
             // NYI
+        }
+
+        private bool RemoveCondition(Condition condition)
+        {
+            // TODO: 
+            //if (_owner == null) return Logger.WarnReturn(false, "RemoveCondition(): _owner == null");
+
+            if (condition == null) return false;
+
+            // TODO: more checks
+            // TODO: ConditionCollection::unaccrueCondition()
+
+            if (_currentConditionDict.Remove(condition.Id) == false)
+                Logger.Warn($"RemoveCondition(): Failed to remove condition id {condition.Id}");
+
+            return true;
+        }
+
+        // See ConditionCollection::MakeConditionStackId()
+        public readonly struct StackId
+        {
+            public PrototypeId PrototypeRef { get; }    // ConditionPrototype or PowerPrototype
+            public int CreatorPowerIndex { get; }
+            public ulong CreatorId { get; }             // EntityId or PlayerGuid
+
+            public StackId(PrototypeId prototypeRef, int creatorPowerIndex, ulong creatorId)
+            {
+                PrototypeRef = prototypeRef;
+                CreatorPowerIndex = creatorPowerIndex;
+                CreatorId = creatorId;
+            }
         }
     }
 }
