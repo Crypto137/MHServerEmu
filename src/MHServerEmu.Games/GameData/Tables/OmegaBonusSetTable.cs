a4ec7bcd9524c4eb4ae7092827fd53db97778438
Crypto137 <Crypto137@users.noreply.github.com>
2024-04-23 22:09:35 +0300

    Implement OmegaBonusSetTable

--- src/MHServerEmu.Games/GameData/Tables/OmegaBonusSetTable.cs
+++ src/MHServerEmu.Games/GameData/Tables/OmegaBonusSetTable.cs
@@ -1,6 +1,30 @@
-﻿namespace MHServerEmu.Games.GameData.Tables
+﻿using MHServerEmu.Games.GameData.Prototypes;
+
+namespace MHServerEmu.Games.GameData.Tables
 {
     public class OmegaBonusSetTable
     {
+        private readonly Dictionary<PrototypeId, OmegaBonusSetPrototype> _omegaBonusSetDict = new();
+
+        public OmegaBonusSetTable()
+        {
+            AdvancementGlobalsPrototype advGlobalsProto = GameDatabase.AdvancementGlobalsPrototype;
+
+            foreach (var omegaBonusSetRef in advGlobalsProto.OmegaBonusSets)
+            {
+                var omegaBonusSetProto = omegaBonusSetRef.As<OmegaBonusSetPrototype>();
+
+                foreach (var omegaBonusRef in omegaBonusSetProto.OmegaBonuses)
+                    _omegaBonusSetDict[omegaBonusRef] = omegaBonusSetProto;
+            }
+        }
+
+        public OmegaBonusSetPrototype GetOmegaBonusSet(PrototypeId omegaBonusRef)
+        {
+            if (_omegaBonusSetDict.TryGetValue(omegaBonusRef, out OmegaBonusSetPrototype omegaBonusSet) == false)
+                return null;
+
+            return omegaBonusSet;
+        }
     }
 }
