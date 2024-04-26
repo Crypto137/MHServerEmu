feac29615d272feaf972dfcdcad93b40774acffe
AlexBond <AlexBondLeo@gmail.com>
2024-04-26 16:58:13 +0300

    NaviPath

---
+++ src/MHServerEmu.Games/Navi/NaviPath.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace MHServerEmu.Games.Navi
+{
+    public class NaviPath
+    {
+        private List<NaviPathNode> _pathNodes;
+        public bool IsValid { get =>  _pathNodes.Count > 0 ; }
+    }
+
+    public class NaviPathNode
+    {
+    }
+}
