456df587b7e22073361f3725400d305812f4c568
AlexBond <AlexBondLeo@gmail.com>
2024-05-01 22:47:29 +0300

    IAIState interface

---
+++ src/MHServerEmu.Games/Behavior/StaticAI/IAIState.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MHServerEmu.Games.Behavior.StaticAI
+{
+    public interface IAIState
+    {
+        public bool Validate(IStateContext context);
+        public void Start(IStateContext context);
+        public StaticBehaviorReturnType Update(IStateContext context);
+        public void End(AIController ownerController, StaticBehaviorReturnType state);
+    }
+
+    public class IStateContext
+    {
+        public AIController OwnerController;
+        public IStateContext(AIController ownerController)
+        {
+            OwnerController = ownerController;
+        }
+    }
+
+    public enum StaticBehaviorReturnType
+    {
+        None,
+    }
+}
--- src/MHServerEmu.Games/Behavior/StaticAI/MoveTo.cs
+++ src/MHServerEmu.Games/Behavior/StaticAI/MoveTo.cs
@@ -1,8 +1,33 @@
 ﻿
-
 namespace MHServerEmu.Games.Behavior.StaticAI
 {
-    public class MoveTo
+    public class MoveTo : IAIState
     {
+        public void End(AIController ownerController, StaticBehaviorReturnType state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(IStateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StaticBehaviorReturnType Update(IStateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(IStateContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MoveToContext : IStateContext
+    {
+        public MoveToContext(AIController ownerController) : base(ownerController)
+        {
+        }
     }
 }
