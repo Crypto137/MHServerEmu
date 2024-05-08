37ee0a0a8ccd30e1dd40aaf1ff25104157848624
AlexBond <AlexBondLeo@gmail.com>
2024-05-08 04:55:57 +0300

    IAIState classes

---
+++ src/MHServerEmu.Games/Behavior/StaticAI/Rotate.cs
@@ -0,0 +1,26 @@
+﻿
+namespace MHServerEmu.Games.Behavior.StaticAI
+{
+    public class Rotate : IAIState
+    {
+        public void End(AIController ownerController, StaticBehaviorReturnType state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(IStateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StaticBehaviorReturnType Update(IStateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(IStateContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
