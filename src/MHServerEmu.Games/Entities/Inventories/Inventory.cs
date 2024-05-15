045c7ec098c7bfc040d6ffcf93bf7a10036dc06f
Crypto137 <Crypto137@users.noreply.github.com>
2024-05-15 05:38:08 +0300

    Implement InventoryCollection

--- src/MHServerEmu.Games/Entities/Inventories/Inventory.cs
+++ src/MHServerEmu.Games/Entities/Inventories/Inventory.cs
@@ -8,7 +8,36 @@ namespace MHServerEmu.Games.Entities.Inventories
     {
         private static readonly Logger Logger = LogManager.CreateLogger();
 
-        // this is just a stub for now
+        public Game Game { get; }
+        public ulong OwnerId { get; private set; }
+        public Entity Owner { get => Game.EntityManager.GetEntity<Entity>(OwnerId); }
+
+        public InventoryPrototype Prototype { get; private set; }
+        public PrototypeId PrototypeDataRef { get => Prototype != null ? Prototype.DataRef : PrototypeId.Invalid; }
+
+        public InventoryCategory Category { get; private set; } = InventoryCategory.None;
+        public ConvenienceLabel ConvenienceLabel { get; private set; } = ConvenienceLabel.None;
+        public int MaxCapacity { get; private set; }
+
+        public Inventory(Game game)
+        {
+            Game = game;
+        }
+
+        public bool Initialize(PrototypeId prototypeRef, ulong ownerId)
+        {
+            var prototype = prototypeRef.As<InventoryPrototype>();
+            if (prototype == null) return Logger.WarnReturn(false, "Initialize(): prototype == null");
+
+            Prototype = prototype;
+            OwnerId = ownerId;
+            Category = prototype.Category;
+            ConvenienceLabel = prototype.ConvenienceLabel;
+            MaxCapacity = prototype.CapacityUnlimited ? int.MaxValue : prototype.Capacity;
+
+            if (ownerId == EntityManager.InvalidEntityId) return Logger.WarnReturn(false, "Initialize(): ownerId == InvalidEntityId");
+            return true;
+        }
 
         public static bool IsPlayerStashInventory(PrototypeId inventoryRef)
         {
