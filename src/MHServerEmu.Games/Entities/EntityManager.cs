971d8d0826cdb8ae87c1d8cc8880f0f96b4d1988
Crypto137 <Crypto137@users.noreply.github.com>
2024-05-27 14:06:43 +0300

    Move entity id validation for GetEntity() to the private method

--- src/MHServerEmu.Games/Entities/EntityManager.cs
+++ src/MHServerEmu.Games/Entities/EntityManager.cs
@@ -287,9 +287,7 @@ namespace MHServerEmu.Games.Entities
 
         public T GetEntity<T>(ulong entityId, GetEntityFlags flags = GetEntityFlags.None) where T : Entity
         {
-            // This validation happens here rather than in the private method because the private method
-            // is used in CreateEntity() to check for id/dbGuid collisions.
-            if (entityId == Entity.InvalidId) return Logger.WarnReturn<T>(null, "GetEntity(): entityId == Entity.InvalidId");
+            if (entityId == Entity.InvalidId) return null;
 
             // Prevent destroyed entities from being accessed externally.
             return GetEntity(entityId, flags & ~GetEntityFlags.DestroyedOnly) as T;
@@ -361,6 +359,9 @@ namespace MHServerEmu.Games.Entities
 
         private Entity GetEntity(ulong entityId, GetEntityFlags flags)
         {
+            // We should have a valid entity id by this point
+            if (entityId == Entity.InvalidId) return Logger.WarnReturn<Entity>(null, "GetEntity(): entityId == Entity.InvalidId");
+
             if (_entityDict.TryGetValue(entityId, out Entity entity) && ValidateEntityForGet(entity, flags))
                 return entity;
 
