f649d5854f9da162e56d1b3a9ce66279812bc872
Crypto137 <Crypto137@users.noreply.github.com>
2024-06-10 18:08:48 +0300

    Add command group for AOI commands

--- src/MHServerEmu/Commands/Implementations/PlayerCommands.cs
+++ src/MHServerEmu/Commands/Implementations/PlayerCommands.cs
@@ -1,6 +1,5 @@
 ﻿using MHServerEmu.Commands.Attributes;
 using MHServerEmu.Core.Config;
-using MHServerEmu.Core.Extensions;
 using MHServerEmu.DatabaseAccess.Models;
 using MHServerEmu.Frontend;
 using MHServerEmu.Games;
@@ -31,28 +30,6 @@ namespace MHServerEmu.Commands.Implementations
             playerConnection.Player.SwitchAvatar((PrototypeId)avatar, out _);
             game.MovePlayerToRegion(playerConnection, playerConnection.RegionDataRef, playerConnection.WaypointDataRef);
             return $"Changing avatar to {avatar}.";
-        }
-
-        [Command("aoi", "Changes player AOI volume size.\nUsage: player aoi [value]", AccountUserLevel.User)]
-        public string AOI(string[] @params, FrontendClient client)
-        {
-            if (client == null) return "You can only invoke this command from the game.";
-
-            CommandHelper.TryGetPlayerConnection(client, out PlayerConnection playerConnection);
-
-            if (@params.Length == 0) return $"Current AOI volume = {playerConnection.AOI.AOIVolume}.";
-
-            if (@params[0].ToLower() == "reset")
-            {
-                playerConnection.AOI.AOIVolume = 3200;
-                return $"Resetting player AOI volume size to {playerConnection.AOI.AOIVolume}.";
-            }
-
-            if ((int.TryParse(@params[0], out int volume) && volume.IsWithin(1600, 5000)) == false)
-                return $"Failed to change AOI volume size to {@params[0]}. Available range [1600..5000]";
-
-            playerConnection.AOI.AOIVolume = volume;
-            return $"Changed player AOI volume size to {volume}.";
         }
 
         [Command("costume", "Changes costume for the current avatar.\nUsage: player costume [name|reset|default]", AccountUserLevel.User)]
