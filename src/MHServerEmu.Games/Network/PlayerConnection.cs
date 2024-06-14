a8312b45c6b71a3ac32c3efdac8dcfcdeca18ee8
Crypto137 <Crypto137@users.noreply.github.com>
2024-06-14 12:45:37 +0300

    Switch transitions to using ChangeRegionPosition()

--- src/MHServerEmu.Games/Network/PlayerConnection.cs
+++ src/MHServerEmu.Games/Network/PlayerConnection.cs
@@ -562,17 +562,9 @@ namespace MHServerEmu.Games.Network
 
                 uint cellId = target.Properties[PropertyEnum.MapCellId];
                 uint areaId = target.Properties[PropertyEnum.MapAreaId];
-                Logger.Trace($"Teleporting to areaid {areaId} cellid {cellId}");
+                Logger.Trace($"Teleporting to areaId={areaId} cellId={cellId}");
 
-                SendMessage(NetMessageEntityPosition.CreateBuilder()
-                    .SetIdEntity(Player.CurrentAvatar.Id)
-                    .SetFlags((uint)ChangePositionFlags.Teleport)
-                    .SetPosition(targetPos.ToNetStructPoint3())
-                    .SetOrientation(targetRot.ToNetStructPoint3())
-                    .SetCellId(cellId)
-                    .SetAreaId(areaId)
-                    .SetEntityPrototypeId((ulong)Player.CurrentAvatar.PrototypeDataRef)
-                    .Build());
+                Player.CurrentAvatar.ChangeRegionPosition(targetPos, targetRot, ChangePositionFlags.Teleport);
 
                 LastPosition = targetPos;
             }
