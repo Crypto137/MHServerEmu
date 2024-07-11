a6cde96d2b89b90f409667e56010b6bf9c9a411b
Crypto137 <Crypto137@users.noreply.github.com>
2024-07-12 00:52:49 +0300

    Overhaul LiveTuningManager

--- src/MHServerEmu.Games/GameData/LiveTuning/LiveTuningData.cs
+++ src/MHServerEmu.Games/GameData/LiveTuning/LiveTuningData.cs
@@ -1,5 +1,4 @@
 ﻿using Gazillion;
-using MHServerEmu.Core.Helpers;
 using MHServerEmu.Core.Logging;
 
 namespace MHServerEmu.Games.GameData.LiveTuning
@@ -8,42 +7,36 @@ namespace MHServerEmu.Games.GameData.LiveTuning
     {
         private static readonly Logger Logger = LogManager.CreateLogger();
 
-        private readonly List<LiveTuningSetting> _settingList;
+        public int ChangeNum { get; set; } = 0;
 
-        public int Count { get => _settingList.Count; }
-
-        /// <summary>
-        /// Initializes a new empty LiveTuningData instance.
-        /// </summary>
         public LiveTuningData()
         {
-            _settingList = new();
+
         }
 
-        /// <summary>
-        /// Initializes a new LiveTuningData instance from the specified JSON file.
-        /// </summary>
-        public LiveTuningData(string jsonPath)
+        public void ResetToDefaults()
         {
-            _settingList = FileHelper.DeserializeJson<List<LiveTuningSetting>>(jsonPath);
-            foreach (var setting in _settingList)
-                Logger.Trace(setting.ToString());
-        }
-        
-        /// <summary>
-        /// Generates an update message from the current live tuning data.
-        /// </summary>
-        public NetMessageLiveTuningUpdate ToNetMessageLiveTuningUpdate()
-        {
-            return NetMessageLiveTuningUpdate.CreateBuilder()
-                .AddRangeTuningTypeKeyValueSettings(_settingList.Select(setting => setting.ToNetStructProtoEnumValue()))
-                .Build();
+
         }
 
-        public void PrintTuningVars()
+        public void Copy(LiveTuningData target)
         {
-            foreach (LiveTuningSetting setting in _settingList)
-                Logger.Trace(setting.ToString());
+
+        }
+
+        public void UpdateLiveTuningVar(PrototypeId tuningVarProtoRef, int tuningVarEnum, float tuningVarValue)
+        {
+
+        }
+
+        public void UpdateLiveGlobalTuningVar(GlobalTuningVar tuningVarEnum, float tuningVarValue)
+        {
+
+        }
+
+        public NetMessageLiveTuningUpdate GetLiveTuningUpdate()
+        {
+            return NetMessageLiveTuningUpdate.DefaultInstance;
         }
     }
 }
--- src/MHServerEmu.Games/GameData/LiveTuning/LiveTuningManager.cs
+++ src/MHServerEmu.Games/GameData/LiveTuning/LiveTuningManager.cs
@@ -1,22 +1,168 @@
-﻿using MHServerEmu.Core.Helpers;
+﻿using Gazillion;
+using MHServerEmu.Core.Helpers;
 using MHServerEmu.Core.Logging;
+using MHServerEmu.Games.GameData.Prototypes;
 
 namespace MHServerEmu.Games.GameData.LiveTuning
 {
-    public static class LiveTuningManager
+    public class LiveTuningManager
     {
         private static readonly Logger Logger = LogManager.CreateLogger();
 
-        public static bool IsInitialized { get; }
-        public static LiveTuningData LiveTuningData { get; }
+        private LiveTuningData _liveTuningData = new();
+        private int _lastUpdateChangeNum = 0;
 
-        // TODO: Make LiveTuningData per game and have games copy it from the manager
+        public static LiveTuningManager Instance { get; } = new();
 
-        static LiveTuningManager()
+        private LiveTuningManager() { }
+
+        public bool Initialize()
         {
-            LiveTuningData = new(Path.Combine(FileHelper.DataDirectory, "Game", "LiveTuningData.json"));
-            Logger.Info($"Loaded {LiveTuningData.Count} live tuning settings");
-            IsInitialized = true;
+            string savedLiveTuningDataPath = Path.Combine(FileHelper.DataDirectory, "Game", "LiveTuningData.json");
+
+            if (File.Exists(savedLiveTuningDataPath))
+            {
+                var updateValues = FileHelper.DeserializeJson<LiveTuningUpdateValue[]>(savedLiveTuningDataPath);
+                UpdateLiveTuningData(updateValues.Select(value => value.ToProtobuf()), true);
+                Logger.Info($"Loaded {updateValues.Length} live tuning settings");
+            }
+
+            return true;
+        }
+
+        public void UpdateLiveTuningData(IEnumerable<NetStructLiveTuningSettingProtoEnumValue> protoEnumValues, bool resetToDefaults)
+        {
+            lock (_liveTuningData)
+            {
+                if (resetToDefaults)
+                    _liveTuningData.ResetToDefaults();
+
+                foreach (NetStructLiveTuningSettingProtoEnumValue protoEnumValue in protoEnumValues)
+                {
+                    PrototypeGuid tuningVarProtoId = (PrototypeGuid)protoEnumValue.TuningVarProtoId;
+                    int tuningVarEnum = protoEnumValue.TuningVarEnum;
+                    float tuningVarValue = protoEnumValue.TuningVarValue;
+
+                    if (tuningVarProtoId != PrototypeGuid.Invalid)
+                    {
+                        PrototypeId tuningVarProtoRef = GameDatabase.GetDataRefByPrototypeGuid(tuningVarProtoId);
+
+                        if (tuningVarProtoRef == PrototypeId.Invalid)
+                        {
+                            Logger.Warn($"UpdateLiveTuningData(): Attempted to Update Live Tuning Setting for a prototype not in the GameDatabase.  Prototype: {tuningVarProtoId}");
+                            continue;
+                        }
+
+                        _liveTuningData.UpdateLiveTuningVar(tuningVarProtoRef, tuningVarEnum, tuningVarValue);
+                        Logger.Trace($"Updated Live Tuning Setting on this GIS.  Prototype: {tuningVarProtoRef.GetName()}  Enumeration: {GetEnumName(tuningVarEnum, tuningVarProtoRef)} Value: {tuningVarValue}");
+                    }
+                    else
+                    {
+                        _liveTuningData.UpdateLiveGlobalTuningVar((GlobalTuningVar)tuningVarEnum, tuningVarValue);
+                        Logger.Trace($"Updated Live Tuning Setting on this GIS.  Prototype: GLOBAL  Enumeration: {(GlobalTuningVar)tuningVarEnum} Value: {tuningVarValue}");
+                    }
+                }
+
+                _liveTuningData.ChangeNum = ++_lastUpdateChangeNum;
+            }
+        }
+
+        public bool CopyLiveTuningData(LiveTuningData target)
+        {
+            if (target.ChangeNum == _lastUpdateChangeNum)
+                return false;
+
+            lock (_liveTuningData)
+            {
+                if (_liveTuningData.ChangeNum != _lastUpdateChangeNum)
+                    Logger.Warn("CopyLiveTuningData(): _liveTuningData.ChangeNum != _lastUpdateChangeNum");
+
+                _liveTuningData.Copy(target);
+                return true;
+            }
+        }
+
+        public static float GetLiveGlobalTuningVar(GlobalTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLiveAreaTuningVar(AreaPrototype areaProto, AreaTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLiveWorldEntityTuningVar(WorldEntityPrototype worldEntityProto, WorldEntityTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLiveAvatarTuningVar(AvatarPrototype avatarProto, AvatarEntityTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLivePopObjTuningVar(PopulationObjectPrototype popObjProto, PopObjTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLivePowerTuningVar(PowerPrototype powerProto, PowerTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLiveRegionTuningVar(RegionPrototype regionProto, RegionTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLiveLootTableTuningVar(LootTablePrototype lootTableProto, LootTableTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLiveMissionTuningVar(MissionPrototype missionProto, MissionTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLiveConditionTuningVar(ConditionPrototype conditionProto, ConditionTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLivePublicEventTuningVar(PublicEventPrototype publicEventProto, PublicEventTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        public static float GetLiveMetricsFrequencyTuningVar(MetricsFrequencyPrototype metricsFrequencyProto, MetricsFrequencyTuningVar tuningVarEnum)
+        {
+            return 0f;
+        }
+
+        // TODO
+        // public static void GetLiveLootGroup()
+
+        private static string GetEnumName(int tuningVarEnum, PrototypeId tuningVarProtoRef = PrototypeId.Invalid)
+        {
+            Prototype prototype = GameDatabase.GetPrototype<Prototype>(tuningVarProtoRef);
+            return prototype switch
+            {
+                AreaPrototype               => ((AreaTuningVar)tuningVarEnum).ToString(),
+                AvatarPrototype             => ((AvatarEntityTuningVar)tuningVarEnum).ToString(),
+                WorldEntityPrototype        => ((WorldEntityTuningVar)tuningVarEnum).ToString(),
+                PopulationObjectPrototype   => ((PopObjTuningVar)tuningVarEnum).ToString(),
+                PowerPrototype              => ((PowerTuningVar)tuningVarEnum).ToString(),
+                RegionPrototype             => ((RegionTuningVar)tuningVarEnum).ToString(),
+                LootTablePrototype          => ((LootTableTuningVar)tuningVarEnum).ToString(),
+                MissionPrototype            => ((MissionTuningVar)tuningVarEnum).ToString(),
+                ConditionPrototype          => ((ConditionTuningVar)tuningVarEnum).ToString(),
+                PublicEventPrototype        => ((PublicEventTuningVar)tuningVarEnum).ToString(),
+                MetricsFrequencyPrototype   => ((MetricsFrequencyTuningVar)tuningVarEnum).ToString(),
+                _                           => tuningVarEnum.ToString()
+            }; ;
         }
     }
 }
