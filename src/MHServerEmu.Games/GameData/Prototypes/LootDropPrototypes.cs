1435d8f57f8899627ea370cfb44a5690dcbaaa7e
Crypto137 <Crypto137@users.noreply.github.com>
2024-07-12 23:19:55 +0300

    Break up loot table prototypes into multiple files matching the client

---
+++ src/MHServerEmu.Games/GameData/Prototypes/LootDropPrototypes.cs
@@ -0,0 +1,95 @@
+﻿using MHServerEmu.Games.Loot;
+
+namespace MHServerEmu.Games.GameData.Prototypes
+{
+    public class LootDropAgentPrototype : LootDropPrototype
+    {
+        public PrototypeId Agent { get; protected set; }
+    }
+
+    public class LootDropCharacterTokenPrototype : LootNodePrototype
+    {
+        public CharacterTokenType AllowedTokenType { get; protected set; }
+        public CharacterFilterType FilterType { get; protected set; }
+        public LootNodePrototype OnTokenUnavailable { get; protected set; }
+    }
+
+    public class LootDropClonePrototype : LootNodePrototype
+    {
+        public LootMutationPrototype[] Mutations { get; protected set; }
+        public short SourceIndex { get; protected set; }
+    }
+
+    public class LootDropCreditsPrototype : LootNodePrototype
+    {
+        public CurveId Type { get; protected set; }
+    }
+
+    public class LootDropItemPrototype : LootDropPrototype
+    {
+        public PrototypeId Item { get; protected set; }
+        public LootMutationPrototype[] Mutations { get; protected set; }
+    }
+
+    public class LootDropItemFilterPrototype : LootDropPrototype
+    {
+        public short ItemRank { get; protected set; }
+        public EquipmentInvUISlot UISlot { get; protected set; }
+    }
+
+    public class LootDropPowerPointsPrototype : LootDropPrototype
+    {
+    }
+
+    public class LootDropHealthBonusPrototype : LootDropPrototype
+    {
+    }
+
+    public class LootDropEnduranceBonusPrototype : LootDropPrototype
+    {
+    }
+
+    public class LootDropXPPrototype : LootNodePrototype
+    {
+        public CurveId XPCurve { get; protected set; }
+    }
+
+    public class LootDropRealMoneyPrototype : LootDropPrototype
+    {
+        public LocaleStringId CouponCode { get; protected set; }
+        public PrototypeId TransactionContext { get; protected set; }
+    }
+
+    public class LootDropBannerMessagePrototype : LootNodePrototype
+    {
+        public PrototypeId BannerMessage { get; protected set; }
+    }
+
+    public class LootDropUsePowerPrototype : LootNodePrototype
+    {
+        public PrototypeId Power { get; protected set; }
+    }
+
+    public class LootDropPlayVisualEffectPrototype : LootNodePrototype
+    {
+        public AssetId RecipientVisualEffect { get; protected set; }
+        public AssetId DropperVisualEffect { get; protected set; }
+    }
+
+    public class LootDropChatMessagePrototype : LootNodePrototype
+    {
+        public LocaleStringId ChatMessage { get; protected set; }
+        public PlayerScope MessageScope { get; protected set; }
+    }
+
+    public class LootDropVanityTitlePrototype : LootNodePrototype
+    {
+        public PrototypeId VanityTitle { get; protected set; }
+    }
+
+    public class LootDropVendorXPPrototype : LootNodePrototype
+    {
+        public PrototypeId Vendor { get; protected set; }
+        public int XP { get; protected set; }
+    }
+}
---
+++ src/MHServerEmu.Games/GameData/Prototypes/LootRollModifierPrototype.cs
@@ -0,0 +1,167 @@
+﻿using MHServerEmu.Games.Missions;
+
+namespace MHServerEmu.Games.GameData.Prototypes
+{
+    public class LootRollModifierPrototype : Prototype
+    {
+    }
+
+    public class LootRollClampLevelPrototype : LootRollModifierPrototype
+    {
+        public int LevelMin { get; protected set; }
+        public int LevelMax { get; protected set; }
+    }
+
+    public class LootRollRequireLevelPrototype : LootRollModifierPrototype
+    {
+        public int LevelMin { get; protected set; }
+        public int LevelMax { get; protected set; }
+    }
+
+    public class LootRollMarkSpecialPrototype : LootRollModifierPrototype
+    {
+    }
+
+    public class LootRollUnmarkSpecialPrototype : LootRollModifierPrototype
+    {
+    }
+
+    public class LootRollMarkRarePrototype : LootRollModifierPrototype
+    {
+    }
+
+    public class LootRollUnmarkRarePrototype : LootRollModifierPrototype
+    {
+    }
+
+    public class LootRollOffsetLevelPrototype : LootRollModifierPrototype
+    {
+        public int LevelOffset { get; protected set; }
+    }
+
+    public class LootRollOnceDailyPrototype : LootRollModifierPrototype
+    {
+        public bool PerAccount { get; protected set; }
+    }
+
+    public class LootRollCooldownOncePerRolloverPrototype : LootRollModifierPrototype
+    {
+        public bool PerAccount { get; protected set; }
+    }
+
+    public class LootRollCooldownByChannelPrototype : LootRollModifierPrototype
+    {
+        public bool PerAccount { get; protected set; }
+    }
+
+    public class LootRollSetAvatarPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId Avatar { get; protected set; }
+    }
+
+    public class LootRollSetItemLevelPrototype : LootRollModifierPrototype
+    {
+        public int Level { get; protected set; }
+    }
+
+    public class LootRollModifyAffixLimitsPrototype : LootRollModifierPrototype
+    {
+        public AffixPosition Position { get; protected set; }
+        public short ModifyMinBy { get; protected set; }
+        public short ModifyMaxBy { get; protected set; }
+        public PrototypeId Category { get; protected set; }
+    }
+
+    public class LootRollSetRarityPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Choices { get; protected set; }
+    }
+
+    public class LootRollSetUsablePrototype : LootRollModifierPrototype
+    {
+        public float Usable { get; protected set; }
+    }
+
+    public class LootRollUseLevelVerbatimPrototype : LootRollModifierPrototype
+    {
+        public bool UseLevelVerbatim { get; protected set; }
+    }
+
+    public class LootRollRequireDifficultyTierPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Choices { get; protected set; }
+    }
+
+    public class LootRollModifyDropByDifficultyTierPrototype : LootRollModifierPrototype
+    {
+        public CurveId ModifierCurve { get; protected set; }
+    }
+
+    public class LootRollRequireConditionKeywordPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Choices { get; protected set; }
+    }
+
+    public class LootRollForbidConditionKeywordPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Choices { get; protected set; }
+    }
+
+    public class LootRollRequireDropperKeywordPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Choices { get; protected set; }
+    }
+
+    public class LootRollForbidDropperKeywordPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Choices { get; protected set; }
+    }
+
+    public class LootRollRequireRegionKeywordPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Choices { get; protected set; }
+    }
+
+    public class LootRollForbidRegionKeywordPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Choices { get; protected set; }
+    }
+
+    public class LootRollRequireRegionScenarioRarityPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Choices { get; protected set; }
+    }
+
+    public class LootRollRequireKillCountPrototype : LootRollModifierPrototype
+    {
+        public int KillsRequired { get; protected set; }
+    }
+
+    public class LootRollRequireWeekdayPrototype : LootRollModifierPrototype
+    {
+        public Weekday[] Choices { get; protected set; }
+    }
+
+    public class LootRollIgnoreCooldownPrototype : LootRollModifierPrototype
+    {
+    }
+
+    public class LootRollIgnoreVendorXPCapPrototype : LootRollModifierPrototype
+    {
+    }
+
+    public class LootRollSetRegionAffixTablePrototype : LootRollModifierPrototype
+    {
+        public PrototypeId RegionAffixTable { get; protected set; }
+    }
+
+    public class LootRollIncludeCurrencyBonusPrototype : LootRollModifierPrototype
+    {
+    }
+
+    public class LootRollMissionStateRequiredPrototype : LootRollModifierPrototype
+    {
+        public PrototypeId[] Missions { get; protected set; }
+        public MissionState RequiredState { get; protected set; }
+    }
+}
