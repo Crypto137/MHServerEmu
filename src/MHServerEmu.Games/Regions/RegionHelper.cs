d04401643b949b1892902625f90d574950ed2657
Crypto137 <Crypto137@users.noreply.github.com>
2024-07-24 15:02:16 +0300

    Merge RegionDumper with RegionHelper

--- src/MHServerEmu.Games/Regions/RegionHelper.cs
+++ src/MHServerEmu.Games/Regions/RegionHelper.cs
@@ -1,4 +1,6 @@
-﻿using MHServerEmu.Games.GameData;
+﻿using MHServerEmu.Core.Helpers;
+using MHServerEmu.Core.VectorMath;
+using MHServerEmu.Games.GameData;
 using MHServerEmu.Games.Missions;
 using MHServerEmu.Games.Properties;
 using MHServerEmu.Games.UI.Widgets;
@@ -315,5 +317,48 @@ namespace MHServerEmu.Games.Regions
             }
         }
 
+        public static void DumpRegionToJson(Region region)
+        {
+            RegionDump regionDump = new();
+
+            foreach (var areaKvp in region.Areas)
+            {
+                AreaDump areaDump = new(areaKvp.Value.PrototypeDataRef, areaKvp.Value.Origin);
+                regionDump.Add(areaKvp.Key, areaDump);
+
+                foreach (var cellKvp in areaKvp.Value.Cells)
+                    areaDump.Cells.Add(cellKvp.Key, new(cellKvp.Value.Prototype.ToString(), cellKvp.Value.AreaPosition));
+            }
+
+            FileHelper.SerializeJson(Path.Combine(FileHelper.ServerRoot, "RegionDumps", $"{region.PrototypeName}_{region.RandomSeed}.json"),
+                regionDump, new System.Text.Json.JsonSerializerOptions() { WriteIndented = true });
+        }
+
+        private class RegionDump : Dictionary<uint, AreaDump> { }
+
+        private class AreaDump
+        {
+            public ulong PrototypeDataRef { get; set; }
+            public Vector3 Origin { get; set; }
+            public SortedDictionary<uint, CellDump> Cells { get; set; } = new();
+
+            public AreaDump(PrototypeId areaProtoRef, Vector3 origin)
+            {
+                PrototypeDataRef = (ulong)areaProtoRef;
+                Origin = origin;
+            }
+        }
+
+        private class CellDump
+        {
+            public string PrototypeName { get; set; }
+            public Vector3 Position { get; set; }
+
+            public CellDump(string prototypeName, Vector3 position)
+            {
+                PrototypeName = prototypeName;
+                Position = position;
+            }
+        }
     }
 }
