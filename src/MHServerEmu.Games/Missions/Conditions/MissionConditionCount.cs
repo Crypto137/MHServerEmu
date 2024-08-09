4b83c8ddbb5d5644c2bad8d8c51b6825abc4d228
AlexBond <AlexBondLeo@gmail.com>
2024-08-10 01:25:59 +0300

    Implement MissionConditionCount

--- src/MHServerEmu.Games/Missions/Conditions/MissionConditionCount.cs
+++ src/MHServerEmu.Games/Missions/Conditions/MissionConditionCount.cs
@@ -4,9 +4,49 @@ namespace MHServerEmu.Games.Missions.Conditions
 {
     public class MissionConditionCount : MissionConditionList
     {
+        protected MissionConditionCountPrototype Proto => Prototype as MissionConditionCountPrototype;
         public MissionConditionCount(Mission mission, IMissionConditionOwner owner, MissionConditionPrototype prototype) 
             : base(mission, owner, prototype)
         {
+        }
+
+        public override bool IsCompleted()
+        {
+            var proto = Proto;
+            if (proto == null) return false;
+
+            long count = 0;
+            foreach(var condition in Conditions)
+            {
+                long currentCount = 0;
+                long requiredCount = 0;
+                condition.GetCompletionCount(ref currentCount, ref requiredCount, true);
+                count += currentCount;
+
+                if (count >= proto.Count) 
+                    return true;
+            }
+
+            return false;
+        }
+
+        public override bool GetCompletionCount(ref long currentCount, ref long requiredCount, bool isRequired)
+        {
+            var proto = Proto;
+            if (proto == null) return false;
+
+            base.GetCompletionCount(ref currentCount, ref requiredCount, true);
+            requiredCount = proto.Count;
+
+            return requiredCount > 0;
+        }
+
+        public override bool OnConditionCompleted() => false;
+
+        public override void OnUpdateCondition(MissionCondition condition)
+        {
+            base.OnUpdateCondition(condition);
+            base.OnConditionCompleted();
         }
     }
 }
