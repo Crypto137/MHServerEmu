197ed0ee2131e42ea5b4f6ac6932ab855d6c8440
AlexBond <AlexBondLeo@gmail.com>
2024-08-09 18:37:55 +0300

    MissionConditionCohort

--- src/MHServerEmu.Games/Missions/Conditions/MissionConditionCohort.cs
+++ src/MHServerEmu.Games/Missions/Conditions/MissionConditionCohort.cs
@@ -4,9 +4,11 @@ namespace MHServerEmu.Games.Missions.Conditions
 {
     public class MissionConditionCohort : MissionPlayerCondition
     {
+        protected MissionConditionCohortPrototype Proto => Prototype as MissionConditionCohortPrototype;
         public MissionConditionCohort(Mission mission, IMissionConditionOwner owner, MissionConditionPrototype prototype) 
             : base(mission, owner, prototype)
         {
+            // DevelopmentOnly
         }
     }
 }
