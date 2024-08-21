12b35d1e7030353efb43c3cafa75e791e3e618c5
AlexBond <AlexBondLeo@gmail.com>
2024-08-22 02:16:57 +0300

    Implement MissionActionEntitySetState

--- src/MHServerEmu.Games/Missions/Actions/MissionActionEncounterSpawn.cs
+++ src/MHServerEmu.Games/Missions/Actions/MissionActionEncounterSpawn.cs
@@ -5,17 +5,19 @@ namespace MHServerEmu.Games.Missions.Actions
 {
     public class MissionActionEncounterSpawn : MissionAction
     {
+        private MissionActionEncounterSpawnPrototype _proto;
         public MissionActionEncounterSpawn(IMissionActionOwner owner, MissionActionPrototype prototype) : base(owner, prototype)
         {
             // CH07MrSinisterSpawnController
+            _proto = prototype as MissionActionEncounterSpawnPrototype;
         }
 
         public override void Run()
         {
             var popManager = Region?.PopulationManager;
             if (popManager == null) return;
-            if (Prototype is not MissionActionEncounterSpawnPrototype proto) return;
-            popManager.SpawnEncounterPhase(proto.Phase, proto.GetEncounterRef(), proto.MissionSpawnOnly ? MissionRef : PrototypeId.Invalid);
+            var missionRef = _proto.MissionSpawnOnly ? MissionRef : PrototypeId.Invalid;
+            popManager.SpawnEncounterPhase(_proto.Phase, _proto.GetEncounterRef(), missionRef);
         }
     }
 }
