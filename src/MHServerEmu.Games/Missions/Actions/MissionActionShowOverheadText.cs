5c94a349433a9f89a6d158ed8e1356d1e93dc7cc
AlexBond <AlexBondLeo@gmail.com>
2024-08-25 02:40:43 +0300

    Implement MissionActionShowOverheadText

--- src/MHServerEmu.Games/Missions/Actions/MissionActionShowOverheadText.cs
+++ src/MHServerEmu.Games/Missions/Actions/MissionActionShowOverheadText.cs
@@ -1,12 +1,21 @@
+using MHServerEmu.Games.Entities;
 using MHServerEmu.Games.GameData.Prototypes;
 
 namespace MHServerEmu.Games.Missions.Actions
 {
     public class MissionActionShowOverheadText : MissionActionEntityTarget
     {
+        private MissionActionShowOverheadTextPrototype _proto;
         public MissionActionShowOverheadText(IMissionActionOwner owner, MissionActionPrototype prototype) : base(owner, prototype)
         {
             // PunksLoiteringChurch
+            _proto = prototype as MissionActionShowOverheadTextPrototype;
+        }
+
+        public override bool RunEntity(WorldEntity entity)
+        {
+            entity.ShowOverheadText(_proto.DisplayText, _proto.DurationMS / 1000.0f);
+            return true;
         }
 
         public override bool RunOnStart() => false;
