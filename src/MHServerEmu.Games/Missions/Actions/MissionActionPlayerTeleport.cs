ee0ba35615888e1514f340496c009e1bc52d10f8
AlexBond <AlexBondLeo@gmail.com>
2024-08-25 01:12:54 +0300

    Implement MissionActionPlayerTeleport

--- src/MHServerEmu.Games/Missions/Actions/MissionActionPlayerTeleport.cs
+++ src/MHServerEmu.Games/Missions/Actions/MissionActionPlayerTeleport.cs
@@ -1,12 +1,28 @@
+using MHServerEmu.Games.Entities;
+using MHServerEmu.Games.GameData;
 using MHServerEmu.Games.GameData.Prototypes;
 
 namespace MHServerEmu.Games.Missions.Actions
 {
     public class MissionActionPlayerTeleport : MissionAction
     {
+        private MissionActionPlayerTeleportPrototype _proto;
         public MissionActionPlayerTeleport(IMissionActionOwner owner, MissionActionPrototype prototype) : base(owner, prototype)
         {
             // LokiPhase1Controller
+            _proto = prototype as MissionActionPlayerTeleportPrototype;
+        }
+
+        public override void Run()
+        {
+            bool teleportRegion = _proto.TeleportRegionTarget != PrototypeId.Invalid;
+            foreach (Player player in GetDistributors(_proto.SendTo))
+            {
+                if (teleportRegion)
+                    Transition.TeleportToRemoteTarget(player, _proto.TeleportRegionTarget);
+                else
+                    Transition.TeleportToLastTown(player);
+            }
         }
     }
 }
