05b5de22e5ce1b3c9b9a03ee8ba3828be0fe07f7
Crypto137 <Crypto137@users.noreply.github.com>
2024-09-25 23:05:57 +0300

    Optimize UInt24 writing

--- src/MHServerEmu.Core/Extensions/BinaryWriterExtensions.cs
+++ src/MHServerEmu.Core/Extensions/BinaryWriterExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace MHServerEmu.Core.Extensions
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace MHServerEmu.Core.Extensions
 {
     public static class BinaryWriterExtensions
     {
@@ -7,9 +10,15 @@
         /// </summary>
         public static void WriteUInt24(this BinaryWriter writer, int value)
         {
+            // NOTE: This implementation assumes little-endian byte order.
             if (value < 0 || value > 16777215) throw new($"UInt24 overflow for value {value}.");
-            byte[] bytes = BitConverter.GetBytes(value);
-            writer.Write(bytes, 0, 3);
+
+            // Same as BitConverter.TryWriteBytes(), but without the size check
+            Span<byte> bytes = stackalloc byte[sizeof(int)];
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(bytes), value);
+            
+            // The last byte is omitted by slicing the span
+            writer.Write(bytes[..3]);
         }
     }
 }
