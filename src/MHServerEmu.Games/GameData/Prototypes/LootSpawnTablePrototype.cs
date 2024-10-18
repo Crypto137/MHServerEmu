9f94d3cf7a681e6058d93f84a5f6345d8da26871
Crypto137 <Crypto137@users.noreply.github.com>
2024-10-18 06:33:54 +0300

    Implement LootLocationData

--- src/MHServerEmu.Games/GameData/Prototypes/LootSpawnTablePrototype.cs
+++ src/MHServerEmu.Games/GameData/Prototypes/LootSpawnTablePrototype.cs
@@ -1,13 +1,69 @@
-﻿namespace MHServerEmu.Games.GameData.Prototypes
+﻿using MHServerEmu.Core.Collections;
+using MHServerEmu.Core.Extensions;
+using MHServerEmu.Core.Logging;
+using MHServerEmu.Games.Loot;
+
+namespace MHServerEmu.Games.GameData.Prototypes
 {
     public class LootLocationNodePrototype : Prototype
     {
         public short Weight { get; protected set; }
         public LootLocationModifierPrototype[] Modifiers { get; protected set; }
+
+        //---
+
+        public virtual void Roll(LootLocationData lootLocationData)
+        {
+            if (Modifiers.IsNullOrEmpty())
+                return;
+
+            foreach (LootLocationModifierPrototype modProto in Modifiers)
+                modProto.Apply(lootLocationData);
+        }
     }
 
     public class LootLocationTablePrototype : LootLocationNodePrototype
     {
         public LootLocationNodePrototype[] Choices { get; protected set; }
+
+        //---
+
+        private static readonly Logger Logger = LogManager.CreateLogger();
+
+        public override void Roll(LootLocationData lootLocationData)
+        {
+            base.Roll(lootLocationData);
+
+            if (Choices.IsNullOrEmpty())
+            {
+                Logger.Warn($"Roll(): LootSpawnTable has no Choices! {this}");
+                return;
+            }
+
+            LootLocationNodePrototype pick;
+
+            if (Choices.Length == 1)
+            {
+                // If have only one possible choice, just pick it straight away instead of initializing a picker
+                pick = Choices[0];
+            }
+            else
+            {
+                // Pick one of multiple choices
+                Picker<LootLocationNodePrototype> possibleNodes = new(lootLocationData.Game.Random);
+                foreach (LootLocationNodePrototype choiceProto in Choices)
+                    possibleNodes.Add(choiceProto, choiceProto.Weight);
+
+                if (possibleNodes.Empty())
+                {
+                    Logger.Warn($"Roll(): No LootNodePrototypes to pick from! LootSpawnTable: {this}");
+                    return;
+                }
+
+                pick = possibleNodes.Pick();
+            }
+
+            pick.Roll(lootLocationData);
+        }
     }
 }
---
+++ src/MHServerEmu.Games/Loot/LootLocationData.cs
@@ -0,0 +1,50 @@
+﻿using MHServerEmu.Core.Memory;
+using MHServerEmu.Core.VectorMath;
+using MHServerEmu.Games.Entities;
+using MHServerEmu.Games.GameData.Prototypes;
+
+namespace MHServerEmu.Games.Loot
+{
+    public class LootLocationData : IPoolable, IDisposable
+    {
+        // NOTE: This doesn't work properly with random locations, we need to implement some kind
+        // of distribution system that gradually fills space from min radius to max to make use of this.
+
+        private const float DefaultBoundsRadius = 10f;
+
+        public Bounds Bounds { get; } = new();
+        public Game Game { get; private set; }
+        public Vector3 Position { get; private set; }
+        public WorldEntity SourceEntity { get; set; }
+        public float MinRadius { get; set; }
+        public float MaxRadius { get; set; }
+        public bool DropInPlace { get; set; }
+        public Vector3 Offset { get; set; }
+
+        public void Initialize(Game game, Vector3 position, WorldEntity sourceEntity = null)
+        {
+            Game = game;
+            Position = position;
+            SourceEntity = sourceEntity;
+
+            Bounds.InitializeSphere(DefaultBoundsRadius, BoundsCollisionType.Overlapping);
+            Bounds.Center = Position;
+        }
+
+        public void ResetForPool()
+        {
+            // Bounds are reset during initialization
+            Position = default;
+            SourceEntity = default;
+            MinRadius = default;
+            MaxRadius = default;
+            DropInPlace = default;
+            Offset = default;
+        }
+
+        public void Dispose()
+        {
+            ObjectPoolManager.Instance.Return(this);
+        }
+    }
+}
