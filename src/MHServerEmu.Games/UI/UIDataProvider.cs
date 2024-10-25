0d73d2cd05968d3ecdcbc27757fa1ed99efd8be8
AlexBond <AlexBondLeo@gmail.com>
2024-10-26 02:26:28 +0300

    Fix DeleteWidget

--- src/MHServerEmu.Games/UI/UIDataProvider.cs
+++ src/MHServerEmu.Games/UI/UIDataProvider.cs
@@ -95,7 +95,12 @@ namespace MHServerEmu.Games.UI
 
         public void DeleteWidget(PrototypeId widgetRef, PrototypeId contextRef = PrototypeId.Invalid)
         {
-            if (_dataDict.Remove((widgetRef, contextRef)) == false)
+            if (_dataDict.TryGetValue((widgetRef, contextRef), out UISyncData widget))
+            {
+                _dataDict.Remove((widgetRef, contextRef));
+                widget.Deallocate();
+            }
+            else
                 Logger.Warn($"DeleteWidget(): Widget not found, widgetRef={widgetRef}, contextRef={contextRef}");
 
             var region = Region;
