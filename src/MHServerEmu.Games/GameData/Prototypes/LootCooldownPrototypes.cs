541b2cef91586ee4be98a2525619660ad402f804
Crypto137 <Crypto137@users.noreply.github.com>
2024-10-26 19:55:38 +0300

    Implement LootCooldownChannelPrototype

--- src/MHServerEmu.Games/GameData/Prototypes/LootCooldownPrototypes.cs
+++ src/MHServerEmu.Games/GameData/Prototypes/LootCooldownPrototypes.cs
@@ -1,10 +1,15 @@
-﻿using MHServerEmu.Games.GameData.Calligraphy.Attributes;
+﻿using MHServerEmu.Core.Logging;
+using MHServerEmu.Games.Entities;
+using MHServerEmu.Games.GameData.Calligraphy.Attributes;
+using MHServerEmu.Games.Properties;
 
 namespace MHServerEmu.Games.GameData.Prototypes
 {
     public class LootCooldownPrototype : Prototype
     {
         public PrototypeId Channel { get; protected set; }
+
+        //---
 
         [DoNotCopy]
         public virtual PrototypeId CooldownRef { get => PrototypeId.Invalid; }
@@ -14,6 +19,8 @@ namespace MHServerEmu.Games.GameData.Prototypes
     {
         public PrototypeId Entity { get; protected set; }
 
+        //---
+
         [DoNotCopy]
         public override PrototypeId CooldownRef { get => Entity; }
     }
@@ -21,6 +28,8 @@ namespace MHServerEmu.Games.GameData.Prototypes
     public class LootCooldownVendorTypePrototype : LootCooldownPrototype
     {
         public PrototypeId VendorType { get; protected set; }
+
+        //---
 
         [DoNotCopy]
         public override PrototypeId CooldownRef { get => VendorType; }
@@ -30,31 +39,68 @@ namespace MHServerEmu.Games.GameData.Prototypes
     {
         public PrototypeId Entity { get; protected set; }
         public PrototypeId[] LocksOut { get; protected set; }
+
+        //---
     }
 
     public class LootCooldownRolloverTimeEntryPrototype : Prototype
     {
         public float WallClockTime24Hr { get; protected set; }
         public Weekday WallClockTimeDay { get; protected set; }
+
+        //---
+
     }
 
     public class LootCooldownChannelPrototype : Prototype
     {
+        //---
+
+        private static readonly Logger Logger = LogManager.CreateLogger();
+
+        public virtual void UpdateCooldown(Player player, PrototypeId lootDropProtoRef)
+        {
+        }
+
+        public bool IsOnCooldown(Game game, PropertyCollection properties)
+        {
+            return false;
+        }
+
+        public void GetCooldownSettings(Player player, PropertyEnum propertyEnum, out bool activeOnPlayer, out bool activeOnAvatar, out TimeSpan cooldownTime)
+        {
+            activeOnPlayer = false;
+            activeOnAvatar = false;
+            cooldownTime = TimeSpan.Zero;
+
+            Logger.Warn($"GetCooldownSettings(): This cooldown channel doesn't support this functionality!\n{this}");
+        }
+
+        public void SetCooldown(Player player, int count)
+        {
+            Logger.Warn($"SetCooldown(): This cooldown channel doesn't support this functionality!\n{this}");
+        }
     }
 
     public class LootCooldownChannelRollOverPrototype : LootCooldownChannelPrototype
     {
         public LootCooldownRolloverTimeEntryPrototype[] RolloverTimeEntries { get; protected set; }
+
+        //---
     }
 
     public class LootCooldownChannelTimePrototype : LootCooldownChannelPrototype
     {
         public float DurationMinutes { get; protected set; }
+
+        //---
     }
 
     public class LootCooldownChannelCountPrototype : LootCooldownChannelPrototype
     {
         public int MaxDrops { get; protected set; }
         public LootCooldownRolloverTimeEntryPrototype[] RolloverTimeEntries { get; protected set; }
+
+        //---
     }
 }
