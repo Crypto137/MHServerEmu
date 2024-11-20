b39ff51cc5e2e87b6641de9a3d619c6a50f873ad
Crypto137 <Crypto137@users.noreply.github.com>
2024-11-20 07:03:13 +0300

    Fix PropertyInfoPrototype field types

--- src/MHServerEmu.Games/GameData/Prototypes/PropertyInfoPrototype.cs
+++ src/MHServerEmu.Games/GameData/Prototypes/PropertyInfoPrototype.cs
@@ -7,31 +7,33 @@ namespace MHServerEmu.Games.GameData.Prototypes
 {
     public class PropertyInfoPrototype : Prototype
     {
-        private static readonly Logger Logger = LogManager.CreateLogger();
-
-        public long Version { get; protected set; }
-        public AggregationMethod AggMethod { get; protected set; }             // A Property/AggregationMethod.type
-        public double Min { get; protected set; }
-        public double Max { get; protected set; }
-        public DatabasePolicy ReplicateToDatabase { get; protected set; }      // A Property/DatabasePolicy.type
+        public sbyte Version { get; protected set; }
+        public AggregationMethod AggMethod { get; protected set; }
+        public float Min { get; protected set; }
+        public float Max { get; protected set; }
+        public DatabasePolicy ReplicateToDatabase { get; protected set; }
         public bool ReplicateToProximity { get; protected set; }
         public bool ReplicateToParty { get; protected set; }
         public bool ReplicateToOwner { get; protected set; }
         public bool ReplicateToDiscovery { get; protected set; }
         public bool ReplicateForTransfer { get; protected set; }
-        public PropertyDataType Type { get; protected set; }                   // A Property/PropertyType.type
-        public double CurveDefault { get; protected set; }
+        public PropertyDataType Type { get; protected set; }
+        public float CurveDefault { get; protected set; }
         public bool ReplicateToDatabaseAllowedOnItems { get; protected set; }
         public bool ClientOnly { get; protected set; }
         public bool SerializeEntityToPowerPayload { get; protected set; }
         public bool SerializePowerToPowerPayload { get; protected set; }
-        public PrototypeId TooltipText { get; protected set; }                 // Localization/Translations/Properties/PropertyTranslation.defaults
+        public PrototypeId TooltipText { get; protected set; }
         public bool TruncatePropertyValueToInt { get; protected set; }
-        public EvalPrototype Eval { get; protected set; }                      // R Eval/Eval.defaults
+        public EvalPrototype Eval { get; protected set; }
         public bool EvalAlwaysCalculates { get; protected set; }
         public bool SerializeConditionSrcToCondition { get; protected set; }
         public bool ReplicateToTrader { get; protected set; }
-        public PrototypeId ValueDisplayFormat { get; protected set; }          // Localization/Translations/Translation.defaults
+        public PrototypeId ValueDisplayFormat { get; protected set; }
+
+        //---
+
+        private static readonly Logger Logger = LogManager.CreateLogger();
 
         [DoNotCopy]
         public AOINetworkPolicyValues RepNetwork { get; private set; } = AOINetworkPolicyValues.AOIChannelNone;
