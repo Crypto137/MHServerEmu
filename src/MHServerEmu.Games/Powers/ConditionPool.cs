63d62f05240c1e1d68332eba3af2016c07612f2a
Crypto137 <Crypto137@users.noreply.github.com>
2024-12-27 06:55:47 +0300

    Implement dynamic resizing for ConditionPool

--- src/MHServerEmu.Games/Powers/ConditionPool.cs
+++ src/MHServerEmu.Games/Powers/ConditionPool.cs
@@ -4,7 +4,11 @@ namespace MHServerEmu.Games.Powers
 {
     public sealed class ConditionPool
     {
-        private const int MaxCapacity = 2048;
+        // Allocate conditions in chunks of 256 instances, which should more than enough for one player.
+        // Cap the maximum number of chunks at 128, which should be enough for how many players a single game can handle.
+        private const int ChunkSize = 256;
+        private const int MaxChunkCount = 128;
+        private const int MaxCapacity = ChunkSize * MaxChunkCount;
 
         private static readonly Logger Logger = LogManager.CreateLogger();
 
@@ -12,29 +16,28 @@ namespace MHServerEmu.Games.Powers
         private static ConditionPool ThreadInstance;
         public static ConditionPool Instance { get { ThreadInstance ??= new(); return ThreadInstance; } }
 
-        private readonly Stack<Condition> _conditionStack = new(MaxCapacity);
+        private readonly Stack<Condition> _conditionStack = new();
 
+        private int _chunkCount = 0;
+
+        private int _allocatedCount = 0;
         private int _activeCount = 0;
 
-        private ConditionPool()
+        private ConditionPool() { }
+
+        public override string ToString()
         {
-            // Pre-allocate conditions for our capacity
-            for (int i = 0; i < MaxCapacity; i++)
-                _conditionStack.Push(new());
+            return $"chunks={_chunkCount}/{MaxChunkCount}, active={_activeCount}/{_allocatedCount}";
         }
 
         public Condition Get()
         {
             _activeCount++;
 
-            Logger.Debug($"Get(): activeCount={_activeCount}");
+            if (_conditionStack.Count == 0 && AllocateChunk() == false)
+                return Logger.WarnReturn(new Condition(), $"Get(): Exceeded maximum capacity ({this})");
 
-            if (_conditionStack.Count == 0)
-            {
-                Logger.Warn($"Get(): Created a new condition instance (ActiveCount={_activeCount})");
-                return new();
-            }
-
+            //Logger.Debug($"Get(): {this}");
             return _conditionStack.Pop();
         }
 
@@ -42,11 +45,27 @@ namespace MHServerEmu.Games.Powers
         {
             _activeCount--;
 
-            if (_conditionStack.Count >= MaxCapacity)
+            if (_conditionStack.Count >= _allocatedCount)
                 return;
 
             condition.Clear();
             _conditionStack.Push(condition);
+        }
+
+        private bool AllocateChunk()
+        {
+            if (_chunkCount >= MaxChunkCount)
+                return false;
+
+            _chunkCount++;
+            _allocatedCount += ChunkSize;
+
+            _conditionStack.EnsureCapacity(_allocatedCount);
+            for (int i = 0; i < ChunkSize; i++)
+                _conditionStack.Push(new());
+
+            Logger.Trace($"AllocateChunk(): {this}");
+            return true;
         }
     }
 }
