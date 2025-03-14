c1d3f9430537443b8e4a55834d21bbfa08534021
Crypto137 <Crypto137@users.noreply.github.com>
2025-03-14 09:43:45 +0300

    Implement store unlocks for avatars and team-ups

--- src/MHServerEmu.Billing/BillingService.cs
+++ src/MHServerEmu.Billing/BillingService.cs
@@ -177,13 +177,29 @@ namespace MHServerEmu.Billing
                         result = BuyItemResultErrorCodes.BUY_RESULT_ERROR_SUCCESS;
                     break;
 
+                case AvatarPrototype avatarProto:
+                    PrototypeId avatarProtoRef = avatarProto.DataRef;
+                    if (player.HasAvatarFullyUnlocked(avatarProtoRef))
+                        result = BuyItemResultErrorCodes.BUY_RESULT_ERROR_ALREADY_HAVE_AVATAR;
+                    else if (player.UnlockAvatar(avatarProtoRef, true))
+                        result = BuyItemResultErrorCodes.BUY_RESULT_ERROR_SUCCESS;
+                    break;
+
+                case AgentTeamUpPrototype teamUpProto:
+                    PrototypeId teamUpProtoRef = teamUpProto.DataRef;
+                    if (player.IsTeamUpAgentUnlocked(teamUpProtoRef))
+                        result = BuyItemResultErrorCodes.BUY_RESULT_ERROR_ALREADY_HAVE_AVATAR;
+                    else if (player.UnlockTeamUpAgent(teamUpProtoRef, true))
+                        result = BuyItemResultErrorCodes.BUY_RESULT_ERROR_SUCCESS;
+                    break;
+
                 default:
                     // Return error for unhandled SKU types
                     Logger.Warn($"OnBuyItemFromCatalog(): Unimplemented catalog item type {catalogItemProto.GetType().Name} for {catalogItemProto}", LogCategory.MTXStore);
                     break;
             }
 
-            if (result == BuyItemResultErrorCodes.BUY_RESULT_ERROR_UNKNOWN)
+            if (result != BuyItemResultErrorCodes.BUY_RESULT_ERROR_SUCCESS)
                 return result;
 
             // Adjust currency balance (do not allow negative balance in case somebody figures out some kind of exploit to get here)
