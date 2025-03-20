64f2884456be22192607a9085d24ebbf8dbd7da6
Crypto137 <Crypto137@users.noreply.github.com>
2025-03-20 17:37:49 +0300

    Implement PickLiveTuningNodes()

--- src/MHServerEmu.Games/GameData/LiveTuning/LiveTuningData.cs
+++ src/MHServerEmu.Games/GameData/LiveTuning/LiveTuningData.cs
@@ -323,7 +323,7 @@ namespace MHServerEmu.Games.GameData.LiveTuning
             return _updateProtobuf;
         }
 
-        public bool GetLiveLootGroup(int lootGroupNum, out IEnumerable<WorldEntityPrototype> lootGroup)
+        public bool GetLiveLootGroup(int lootGroupNum, out IReadOnlyList<WorldEntityPrototype> lootGroup)
         {
             bool found = _lootGroupDict.TryGetValue(lootGroupNum, out List<WorldEntityPrototype> worldEntityProtoList);
             lootGroup = worldEntityProtoList;
