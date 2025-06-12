4f7d66a3315a62f006b9657a749b533eda765f88
Crypto137 <Crypto137@users.noreply.github.com>
2025-06-12 17:05:11 +0300

    Implement LootTableContainsMutationOfType visitor

---
+++ src/MHServerEmu.Games/Loot/Visitors/LootTableContainsMutationOfType.cs
@@ -0,0 +1,46 @@
+﻿using MHServerEmu.Core.Extensions;
+using MHServerEmu.Games.GameData.Prototypes;
+
+namespace MHServerEmu.Games.Loot.Visitors
+{
+    // This isn't really needed for anything but debugging crafting recipes
+
+    public struct LootTableContainsMutationOfType<T> : ILootTableNodeVisitor where T : LootMutationPrototype
+    {
+        private bool _found = false;
+
+        public LootTableContainsMutationOfType(LootTablePrototype table)
+        {
+            if (table == null)
+                return;
+
+            table.Visit(ref this);
+        }
+
+        public void Visit(LootNodePrototype lootNodeProto)
+        {
+            if (_found)
+                return;
+
+            if (lootNodeProto is not LootDropClonePrototype cloneProto)
+                return;
+
+            if (cloneProto.Mutations.IsNullOrEmpty())
+                return;
+
+            foreach (LootMutationPrototype mutationProto in cloneProto.Mutations)
+            {
+                if (mutationProto is T)
+                {
+                    _found = true;
+                    break;
+                }
+            }
+        }
+
+        public static implicit operator bool(LootTableContainsMutationOfType<T> visitor)
+        {
+            return visitor._found;
+        }
+    }
+}
