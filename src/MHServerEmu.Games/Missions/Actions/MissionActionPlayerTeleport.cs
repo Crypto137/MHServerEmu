1530d96eff66eb3af63bfb16a05cac0525a66b7d
Crypto137 <Crypto137@users.noreply.github.com>
2025-06-27 06:31:41 +0300

    Remove hardcoded prototypes from MissionActionPlayerTeleport

--- src/MHServerEmu.Games/Missions/Actions/MissionActionPlayerTeleport.cs
+++ src/MHServerEmu.Games/Missions/Actions/MissionActionPlayerTeleport.cs
@@ -19,17 +19,11 @@ namespace MHServerEmu.Games.Missions.Actions
             List<Player> players = ListPool<Player>.Instance.Get();
             if (GetDistributors(_proto.SendTo, players))
             {
-                bool teleportRegion = _proto.TeleportRegionTarget != PrototypeId.Invalid;
+                bool hasTarget = _proto.TeleportRegionTarget != PrototypeId.Invalid;
                 foreach (Player player in players)
                 {
-                    if (teleportRegion)
-                    {
-                        if (Mission.PrototypeDataRef == (PrototypeId)2356138960907149996 // TimesBehaviorController
-                            || Mission.PrototypeDataRef == (PrototypeId)3656606685775927811) // RaidSurturFinalPhase
-                            Transition.TeleportToLocalTarget(player, _proto.TeleportRegionTarget);
-                        else
-                            Transition.TeleportToRemoteTarget(player, _proto.TeleportRegionTarget);
-                    }
+                    if (hasTarget)
+                        Transition.TeleportToTarget(player, _proto.TeleportRegionTarget);
                     else
                         Transition.TeleportToLastTown(player);
                 }
