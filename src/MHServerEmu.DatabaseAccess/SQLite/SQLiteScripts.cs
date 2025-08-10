eda9106cbaff19a3d1fb0606f790ee633c71d799
Crypto137 <Crypto137@users.noreply.github.com>
2025-08-10 21:05:42 +0300

    Embed SQL scripts in the DatabaseAccess dll

--- src/MHServerEmu.DatabaseAccess/SQLite/SQLiteScripts.cs
+++ src/MHServerEmu.DatabaseAccess/SQLite/SQLiteScripts.cs
@@ -1,37 +1,34 @@
-﻿using MHServerEmu.Core.Helpers;
-using MHServerEmu.Core.Logging;
+﻿using System.Reflection;
 
 namespace MHServerEmu.DatabaseAccess.SQLite
 {
     public static class SQLiteScripts
     {
-        private static readonly Logger Logger = LogManager.CreateLogger();
-
         public static string GetInitializationScript()
         {
-            string filePath = Path.Combine(FileHelper.DataDirectory, "SQLite", "InitializeDatabase.sql");
-            if (File.Exists(filePath) == false)
-                return Logger.WarnReturn(string.Empty, $"GetDatabaseInitializationScript(): Initialization script file not found at {FileHelper.GetRelativePath(filePath)}");
-
-            return File.ReadAllText(filePath);
+            return LoadScript("InitializeDatabase");
         }
 
         public static string GetMigrationScript(int currentVersion)
         {
-            string filePath = Path.Combine(FileHelper.DataDirectory, "SQLite", "Migrations", $"{currentVersion}.sql");
-            if (File.Exists(filePath) == false)
-                return Logger.WarnReturn(string.Empty, $"GetMigrationScript(): Migration script for version {currentVersion} not found at {FileHelper.GetRelativePath(filePath)}");
-
-            return File.ReadAllText(filePath);
+            return LoadScript($"Migrations.{currentVersion}");
         }
 
         public static string GetLeaderboardsScript()
         {
-            string filePath = Path.Combine(FileHelper.DataDirectory, "SQLite", "InitializeLeaderboardsDatabase.sql");
-            if (File.Exists(filePath) == false)
-                return Logger.WarnReturn(string.Empty, $"GetDatabaseInitializationScript(): Initialization script file not found at {FileHelper.GetRelativePath(filePath)}");
+            return LoadScript("InitializeLeaderboardsDatabase");
+        }
 
-            return File.ReadAllText(filePath);
+        private static string LoadScript(string name)
+        {
+            string resourceName = $"MHServerEmu.DatabaseAccess.SQLite.Scripts.{name}.sql";
+
+            using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+            if (stream == null)
+                throw new Exception($"Script '{name}' not found.");
+
+            using StreamReader reader = new(stream);
+            return reader.ReadToEnd();
         }
     }
 }
