3f6ac87d6eb94a68fa9c3989531fe54a2243f6f4
Crypto137 <Crypto137@users.noreply.github.com>
2025-08-28 18:30:56 +0300

    Remove locks from GroupingClientManager

--- src/MHServerEmu.Grouping/GroupingClientManager.cs
+++ src/MHServerEmu.Grouping/GroupingClientManager.cs
@@ -13,9 +13,8 @@ namespace MHServerEmu.Grouping
         private static readonly Logger Logger = LogManager.CreateLogger();
 
         // Need a name -> client lookup because tells sent by clients are addressed by name.
-        // This class needs to be thread-safe to handle async client connect/disconnect events.
+        // We no longer need thread safety here because everything grouping manager related is now handled by a dedicated worker thread.
 
-        private readonly object _lock = new();
         private readonly Dictionary<ulong, IFrontendClient> _playerDbIdDict = new();
         private readonly Dictionary<string, IFrontendClient> _playerNameDict = new(StringComparer.OrdinalIgnoreCase);   // case insensitive
 
@@ -27,68 +26,57 @@ namespace MHServerEmu.Grouping
 
         public bool AddClient(IFrontendClient client)
         {
-            lock (_lock)
-            {
-                DBAccount account = ((IDBAccountOwner)client).Account;
-                ulong playerDbId = (ulong)account.Id;
-                string playerName = account.PlayerName;
+            DBAccount account = ((IDBAccountOwner)client).Account;
+            ulong playerDbId = (ulong)account.Id;
+            string playerName = account.PlayerName;
 
-                if (_playerDbIdDict.ContainsKey(playerDbId))
-                    return Logger.WarnReturn(false, $"AddClient(): Account {account} is already added");
+            if (_playerDbIdDict.ContainsKey(playerDbId))
+                return Logger.WarnReturn(false, $"AddClient(): Account {account} is already added");
 
-                _playerDbIdDict.Add(playerDbId, client);
-                _playerNameDict.Add(playerName, client); 
+            _playerDbIdDict.Add(playerDbId, client);
+            _playerNameDict.Add(playerName, client); 
 
-                Logger.Info($"Added client [{client}]");
-                return true;
-            }
+            Logger.Info($"Added client [{client}]");
+            return true;
         }
 
         public bool RemoveClient(IFrontendClient client)
         {
-            lock (_lock)
-            {
-                DBAccount account = ((IDBAccountOwner)client).Account;
-                ulong playerDbId = (ulong)account.Id;
-                string playerName = account.PlayerName;
+            DBAccount account = ((IDBAccountOwner)client).Account;
+            ulong playerDbId = (ulong)account.Id;
+            string playerName = account.PlayerName;
 
-                if (_playerDbIdDict.Remove(playerDbId) == false)
-                    return Logger.WarnReturn(false, $"RemoveClient(): Account {account} not found");
+            if (_playerDbIdDict.Remove(playerDbId) == false)
+                return Logger.WarnReturn(false, $"RemoveClient(): Account {account} not found");
 
-                _playerNameDict.Remove(playerName);
+            _playerNameDict.Remove(playerName);
 
-                Logger.Info($"Removed client [{client}]");
-                return true;
-            }
+            Logger.Info($"Removed client [{client}]");
+            return true;
         }
 
         public void OnPlayerNameChanged(ulong playerDbId, string oldPlayerName, string newPlayerName)
         {
-            lock (_lock)
-            {
-                // Update the currently logged in player name lookup
-                if (_playerDbIdDict.TryGetValue(playerDbId, out IFrontendClient client) == false)
-                    return;
+            // Update the currently logged in player name lookup
+            if (_playerDbIdDict.TryGetValue(playerDbId, out IFrontendClient client) == false)
+                return;
 
-                if (_playerNameDict.Remove(oldPlayerName) == false)
-                    Logger.Warn($"OnPlayerNameChanged(): Player 0x{playerDbId:X} is logged in, but doesn't have a name lookup!");
+            if (_playerNameDict.Remove(oldPlayerName) == false)
+                Logger.Warn($"OnPlayerNameChanged(): Player 0x{playerDbId:X} is logged in, but doesn't have a name lookup!");
 
-                _playerNameDict.Add(newPlayerName, client);
+            _playerNameDict.Add(newPlayerName, client);
 
-                Logger.Info($"Updated name for player 0x{playerDbId:X}: {oldPlayerName} => {newPlayerName}");
-            }
+            Logger.Info($"Updated name for player 0x{playerDbId:X}: {oldPlayerName} => {newPlayerName}");
         }
 
         public bool TryGetClient(ulong playerDbId, out IFrontendClient client)
         {
-            lock (_lock)
-                return _playerDbIdDict.TryGetValue(playerDbId, out client);
+            return _playerDbIdDict.TryGetValue(playerDbId, out client);
         }
 
         public bool TryGetClient(string playerName, out IFrontendClient client)
         {
-            lock (_lock)
-                return _playerNameDict.TryGetValue(playerName, out client);
+            return _playerNameDict.TryGetValue(playerName, out client);
         }
 
         public void SendMessage(IMessage message, IFrontendClient client)
@@ -99,25 +87,19 @@ namespace MHServerEmu.Grouping
 
         public void SendMessageFiltered(IMessage message, List<ulong> playerDbIdFilter)
         {
-            lock (_lock)
+            foreach (ulong playerDbId in playerDbIdFilter)
             {
-                foreach (ulong playerDbId in playerDbIdFilter)
-                {
-                    if (_playerDbIdDict.TryGetValue(playerDbId, out IFrontendClient client) == false)
-                        continue;
+                if (_playerDbIdDict.TryGetValue(playerDbId, out IFrontendClient client) == false)
+                    continue;
 
-                    client.SendMessage(MuxChannel, message);
-                }
+                client.SendMessage(MuxChannel, message);
             }
         }
 
         public void SendMessageToAll(IMessage message)
         {
-            lock (_lock)
-            {
-                foreach (IFrontendClient client in _playerDbIdDict.Values)
-                    client.SendMessage(MuxChannel, message);
-            }
+            foreach (IFrontendClient client in _playerDbIdDict.Values)
+                client.SendMessage(MuxChannel, message);
         }
     }
 }
