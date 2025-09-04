2be7b103aa9014c8447483e14a53478febda6de1
Crypto137 <Crypto137@users.noreply.github.com>
2025-09-04 23:50:01 +0300

    Implement PartyMemberInfo management

--- src/MHServerEmu.Games/Social/Parties/Party.cs
+++ src/MHServerEmu.Games/Social/Parties/Party.cs
@@ -1,22 +1,98 @@
-﻿using MHServerEmu.Games.Entities;
+﻿using MHServerEmu.Core.Logging;
+using MHServerEmu.Games.Entities;
+using MHServerEmu.Games.GameData;
 using MHServerEmu.Games.Social.Communities;
 
 namespace MHServerEmu.Games.Social.Parties
 {
     public class Party
     {
+        private static readonly Logger Logger = LogManager.CreateLogger();
+
         private readonly Dictionary<ulong, PartyMemberInfo> _members = new();
 
+        public ulong PartyId { get; }
+        public Gazillion.GroupType Type { get; private set; }
         public ulong LeaderId { get; private set; }
+        public PrototypeId DifficultyTierProtoRef { get; private set; }
+        // string groupPSNSessionId
+
         public int NumMembers { get => _members.Count; }
 
-        public Party()
+        public Party(ulong partyId)
         {
+            PartyId = partyId;
+        }
+
+        public override string ToString()
+        {
+            return $"id=0x{PartyId:X}";
         }
 
         public Dictionary<ulong, PartyMemberInfo>.Enumerator GetEnumerator()
         {
             return _members.GetEnumerator();
+        }
+
+        public void SetFromMessage(Gazillion.PartyInfo protobuf)
+        {
+            LeaderId = protobuf.LeaderDbId;
+            Type = protobuf.Type;
+            DifficultyTierProtoRef = (PrototypeId)protobuf.DifficultyTierProtoId;
+            // groupPSNSessionId
+
+            for (int i = 0; i < protobuf.MembersCount; i++)
+                AddMember(protobuf.MembersList[i]);
+        }
+
+        public bool AddMember(Gazillion.PartyMemberInfo protobuf)
+        {
+            ulong playerDbId = protobuf.PlayerDbId;
+            if (playerDbId == 0) return Logger.WarnReturn(false, "AddMember(): playerDbId == 0");
+
+            if (GetMemberInfo(playerDbId) != null)
+                return UpdateMember(protobuf);
+
+            PartyMemberInfo memberInfo = new();
+            memberInfo.SetFromMsg(protobuf);
+
+            Logger.Info($"Adding new party member. newMember={memberInfo}, party={this}");
+
+            _members[playerDbId] = memberInfo;
+
+            UpdateBoostCounts();
+
+            return true;
+        }
+
+        public bool RemoveMember(ulong memberId, Gazillion.GroupLeaveReason reason)
+        {
+            if (_members.TryGetValue(memberId, out PartyMemberInfo memberInfo) == false)
+            {
+                // This matches the client, but it looks like a bug. Shouldn't boost counts be updating when we actually do a removal?
+                UpdateBoostCounts();
+                return false;
+            }
+
+            Logger.Info($"Removing party member. memberId=0x{memberId:X}, reason={reason}, party={this}");
+
+            _members.Remove(memberId);
+
+            return true;
+        }
+
+        public bool UpdateMember(Gazillion.PartyMemberInfo protobuf)
+        {
+            ulong playerDbId = protobuf.PlayerDbId;
+
+            if (_members.TryGetValue(playerDbId, out PartyMemberInfo memberInfo) == false)
+                return false;
+
+            memberInfo.SetFromMsg(protobuf);
+
+            UpdateBoostCounts();
+
+            return true;
         }
 
         public PartyMemberInfo GetMemberInfo(ulong memberId)
@@ -80,6 +156,11 @@ namespace MHServerEmu.Games.Social.Parties
         public CommunityMember GetCommunityMemberForLeader(Player player)
         {
             return GetCommunityMemberForDbGuid(player, LeaderId);
+        }
+
+        private void UpdateBoostCounts()
+        {
+            // TODO
         }
     }
 }
--- src/MHServerEmu.Games/Social/Parties/PartyMemberInfo.cs
+++ src/MHServerEmu.Games/Social/Parties/PartyMemberInfo.cs
@@ -1,12 +1,48 @@
-﻿namespace MHServerEmu.Games.Social.Parties
+﻿using MHServerEmu.Core.Logging;
+using MHServerEmu.Games.GameData;
+
+namespace MHServerEmu.Games.Social.Parties
 {
     public class PartyMemberInfo
     {
+        private static readonly Logger Logger = LogManager.CreateLogger();
+
         public ulong PlayerDbId { get; private set; }
         public string PlayerName { get; private set; }
-        // repeated uint64 boosts
-        // optional uint64 consoleAccountId
-        // optional uint64 secondaryConsoleAccountId
-        // optional string secondaryPlayerName
+        public HashSet<PrototypeId> Boosts { get; } = new();    // the client uses a sorted map here
+        public ulong ConsoleAccountId { get; private set; }
+        public ulong SecondaryConsoleAccountId { get; private set; }
+        public string SecondaryPlayerName { get; private set; }
+
+        public PartyMemberInfo()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{PlayerName}, PlayerDbGuid=0x{PlayerDbId:X}";
+        }
+
+        public void SetFromMsg(Gazillion.PartyMemberInfo protobuf)
+        {
+            PlayerDbId = protobuf.PlayerDbId;
+            PlayerName = protobuf.PlayerName;
+            ConsoleAccountId = protobuf.HasConsoleAccountId ? protobuf.ConsoleAccountId : 0;
+            SecondaryConsoleAccountId = protobuf.HasSecondaryConsoleAccountId ? protobuf.SecondaryConsoleAccountId : 0;
+            SecondaryPlayerName = protobuf.HasSecondaryPlayerName ? protobuf.SecondaryPlayerName : string.Empty;
+
+            Boosts.Clear();
+            for (int i = 0; i < protobuf.BoostsCount; i++)
+            {
+                PrototypeId boostRef = GameDatabase.GetDataRefByPrototypeGuid((PrototypeGuid)protobuf.BoostsList[i]);
+                if (boostRef == PrototypeId.Invalid)
+                {
+                    Logger.Warn("SetFromMsg(): boostRef == PrototypeId.Invalid");
+                    continue;
+                }
+
+                Boosts.Add(boostRef);
+            }
+        }
     }
 }
