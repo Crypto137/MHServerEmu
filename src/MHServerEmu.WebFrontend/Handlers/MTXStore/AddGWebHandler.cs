dcb03c048602ebb6227eae91fbe268c73b35585c
Crypto137 <Crypto137@users.noreply.github.com>
2025-10-22 21:08:50 +0300

    Add context echo for add G requests

--- src/MHServerEmu.WebFrontend/Handlers/MTXStore/AddGWebHandler.cs
+++ src/MHServerEmu.WebFrontend/Handlers/MTXStore/AddGWebHandler.cs
@@ -1,42 +1,57 @@
-﻿using MHServerEmu.Core.Helpers;
+﻿using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using MHServerEmu.Core.Helpers;
 using MHServerEmu.Core.Logging;
 using MHServerEmu.Core.Network.Web;
-using System.Collections.Specialized;
 
 namespace MHServerEmu.WebFrontend.Handlers.MTXStore
 {
     public class AddGWebHandler : WebHandler
     {
-        private const string HtmlFileName = "add-g.html";
+        private const string HtmlTemplateFileName = "add-g.html";
 
         private static readonly Logger Logger = LogManager.CreateLogger();
-        private static readonly string HtmlFilePath = Path.Combine(FileHelper.DataDirectory, "Web", "MTXStore", HtmlFileName);
+        private static readonly string HtmlTemplateFilePath = Path.Combine(FileHelper.DataDirectory, "Web", "MTXStore", HtmlTemplateFileName);
 
-        private byte[] _htmlData;
+        private readonly string _htmlTemplate;
 
         public AddGWebHandler()
         {
-            if (File.Exists(HtmlFilePath) == false)
+            if (File.Exists(HtmlTemplateFilePath) == false)
             {
-                Logger.Warn($"'{HtmlFileName}' not found, adding Gs via in-game UI will not work");
-                _htmlData = Array.Empty<byte>();
+                Logger.Warn($"'{HtmlTemplateFileName}' not found, adding Gs via in-game UI will not work");
+                _htmlTemplate = string.Empty;
                 return;
             }
 
-            _htmlData = File.ReadAllBytes(HtmlFilePath);
+            _htmlTemplate = File.ReadAllText(HtmlTemplateFilePath);
         }
 
         protected override async Task Post(WebRequestContext context)
         {
-            // TODO
+            if (string.IsNullOrWhiteSpace(_htmlTemplate))
+            {
+                context.StatusCode = (int)HttpStatusCode.InternalServerError;
+                return;
+            }
+
             NameValueCollection request = context.ReadQueryString();
 
             string downloader = request["downloader"];
             string token = request["token"];
             string email = request["email"];
 
+            // TODO: Verify downloader/token/email
             Logger.Debug($"Post(): downloader={downloader}, token={token}, email={email}");
-            await context.SendAsync(_htmlData, "text/html");
+
+            StringBuilder sb = new(_htmlTemplate);
+            sb.Replace("%REQUEST_DOWNLOADER%", downloader);
+            sb.Replace("%REQUEST_TOKEN%", token);
+            sb.Replace("%REQUEST_EMAIL%", email);
+            string html = sb.ToString();
+
+            await context.SendAsync(html, "text/html");
         }
     }
 }
