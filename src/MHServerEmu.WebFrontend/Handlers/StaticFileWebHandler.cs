0ccb95a2d7f2fa9dae955b6c384a71bc2e439150
Crypto137 <Crypto137@users.noreply.github.com>
2026-01-12 20:11:07 +0300

    Add common image MIME types to StaticFileWebHandler

--- src/MHServerEmu.WebFrontend/Handlers/StaticFileWebHandler.cs
+++ src/MHServerEmu.WebFrontend/Handlers/StaticFileWebHandler.cs
@@ -56,6 +56,11 @@ namespace MHServerEmu.WebFrontend.Handlers
                 ".html" => "text/html",
                 ".css"  => "text/css",
                 ".js"   => "text/javascript",
+                ".png"  => "image/png",
+                ".jpg"  => "image/jpeg",
+                ".jpeg" => "image/jpeg",
+                ".gif"  => "image/gif",
+                ".ico"  => "image/x-icon",
                 _       => "application/octet-stream",
             };
         }
