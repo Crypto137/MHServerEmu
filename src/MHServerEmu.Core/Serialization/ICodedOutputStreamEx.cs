1c75103166b110c3173ab2eb3e4d2d8295347a90
Crypto137 <Crypto137@users.noreply.github.com>
2026-03-22 20:37:27 +0300

    Optimize allocations when writing float fields in protobuf messages

---
+++ src/MHServerEmu.Core/Serialization/ICodedOutputStreamEx.cs
@@ -0,0 +1,18 @@
+﻿using Google.ProtocolBuffers;
+
+namespace MHServerEmu.Core.Serialization
+{
+    /// <summary>
+    /// Extended version of <see cref="ICodedOutputStream"/> that exposes additional low level writing functionality.
+    /// </summary>
+    public interface ICodedOutputStreamEx : ICodedOutputStream
+    {
+        void WriteRawVarint32(uint value);
+
+        void WriteRawVarint64(ulong value);
+
+        void WriteRawByte(byte value);
+
+        void WriteRawBytes(byte[] value);
+    }
+}
