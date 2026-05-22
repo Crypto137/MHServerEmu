54306951b975a90f9a035f3837f12a8b8a2cab2b
Crypto137 <Crypto137@users.noreply.github.com>
2026-05-22 07:02:53 +0300

    Clean up and optimize ICanBlock

--- src/MHServerEmu.Games/Powers/MovementPowerEntityCollideFunc.cs
+++ src/MHServerEmu.Games/Powers/MovementPowerEntityCollideFunc.cs
@@ -7,20 +7,17 @@ namespace MHServerEmu.Games.Powers
 {
     public readonly struct MovementPowerEntityCollideFunc : ICanBlock
     {
-        private static readonly Logger Logger = LogManager.CreateLogger();
-
         private readonly int _blockTypeFlags;       // BoundsMovementPowerBlockType
 
         public MovementPowerEntityCollideFunc(int blockTypeFlags)
         {
             _blockTypeFlags = blockTypeFlags;
-            if (_blockTypeFlags == 0)
-                Logger.Warn("Blocking query with movement power blocking type of None doesn't make sense");
+            Verify.IsTrue(_blockTypeFlags != 0, "Blocking query with movement power blocking type of None doesn't make sense.");
         }
 
         public bool CanBlock(WorldEntity otherEntity)
         {
-            var otherEntityProto = otherEntity.WorldEntityPrototype;
+            WorldEntityPrototype otherEntityProto = otherEntity.WorldEntityPrototype;
             BoundsPrototype otherEntityBoundsProto = otherEntityProto?.Bounds;
             
             if (otherEntityBoundsProto == null)
